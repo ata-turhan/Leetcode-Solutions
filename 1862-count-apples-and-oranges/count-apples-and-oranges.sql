260e8a856a9dd9f6172f209996b57a70508a7428
Ata Turhan <ataturhan21@gmail.com>
2024-02-24 19:31:53 -0600

    Time: 2386 ms (12.05%) | Memory: 0B (100.00%) - LeetSync

--- 1862-count-apples-and-oranges/count-apples-and-oranges.sql
+++ 1862-count-apples-and-oranges/count-apples-and-oranges.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
-select sum(b.apple_count + ifnull(c.apple_count, 0)) as apple_count,
-sum(b.orange_count  + ifnull(c.orange_count, 0)) as orange_count
-from boxes as b left join chests as c using(chest_id)
+SELECT 
+    SUM(b.apple_count + COALESCE(c.apple_count, 0)) AS apple_count,
+    SUM(b.orange_count + COALESCE(c.orange_count, 0)) AS orange_count
+FROM 
+    boxes AS b 
+LEFT JOIN 
+    chests AS c 
+USING 
+    (chest_id);
