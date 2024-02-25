26da3912738f472215027f84d6111427b21ea1fe
Ata Turhan <ataturhan21@gmail.com>
2024-02-24 19:34:33 -0600

    Time: 710 ms (73.43%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3300-friday-purchases-i/friday-purchases-i.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT
+    WEEK(purchase_date, 3) - WEEK('2023-11-01', 3) + 1 AS week_of_month,
+    purchase_date,
+    SUM(amount_spend) AS total_amount
+FROM
+    Purchases
+WHERE
+    DAYOFWEEK(purchase_date) = 6
+GROUP BY
+    week_of_month, purchase_date
+ORDER BY
+    week_of_month;
