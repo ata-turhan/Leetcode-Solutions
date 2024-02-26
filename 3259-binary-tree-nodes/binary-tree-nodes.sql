ea1dfea0b1f8dabff4f32a4e66452b01b23a482d
Ata Turhan <ataturhan21@gmail.com>
2024-02-25 19:11:22 -0600

    Time: 471 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

--- 3259-binary-tree-nodes/binary-tree-nodes.sql
+++ 3259-binary-tree-nodes/binary-tree-nodes.sql
@@ -1,17 +1,8 @@
-# Write your MySQL query statement below
-select N, 
-(case
-when
-p is null
-then
-"Root"
-when
-n in (select p from tree)
-then
-"Inner"
-else
-"Leaf"
-end)
-as type
-from tree
-order by N;
+SELECT N,
+    CASE
+        WHEN p IS NULL THEN 'Root'
+        WHEN n IN (SELECT p FROM tree) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM tree
+ORDER BY N;
