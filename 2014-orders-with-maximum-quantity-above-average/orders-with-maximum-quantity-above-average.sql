e1b161cb1a989231a431b009302aeb232427ba05
Ata Turhan <ataturhan21@gmail.com>
2024-03-05 18:55:58 -0600

    Time: 1095 ms (71.95%) | Memory: 0B (100.00%) - LeetSync

--- 2014-orders-with-maximum-quantity-above-average/orders-with-maximum-quantity-above-average.sql
+++ 2014-orders-with-maximum-quantity-above-average/orders-with-maximum-quantity-above-average.sql
@@ -1,18 +1,13 @@
-WITH max_quantity_per_order AS (
-    SELECT order_id, MAX(quantity) AS max_quantity 
-    FROM ordersdetails 
-    GROUP BY order_id
-),
-average_quantity_per_order AS (
-    SELECT order_id, AVG(quantity) AS avg_quantity 
+WITH max_avg_quantity_per_order AS (
+    SELECT order_id, MAX(quantity) AS max_quantity, AVG(quantity) AS avg_quantity 
     FROM ordersdetails 
     GROUP BY order_id
 ),
 max_average_quantity AS (
     SELECT MAX(avg_quantity) AS max_avg_quantity 
-    FROM average_quantity_per_order
+    FROM max_avg_quantity_per_order
 )
 
 SELECT order_id 
-FROM max_quantity_per_order 
+FROM max_avg_quantity_per_order 
 WHERE max_quantity > (SELECT max_avg_quantity FROM max_average_quantity);
