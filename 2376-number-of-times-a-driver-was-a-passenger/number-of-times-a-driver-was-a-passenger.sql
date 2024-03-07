3cbdc9e41b17265dee2c19f389027b5109cc3575
Ata Turhan <ataturhan21@gmail.com>
2024-03-06 18:15:41 -0600

    Time: 934 ms (55.88%) | Memory: 0B (100.00%) - LeetSync

--- 2376-number-of-times-a-driver-was-a-passenger/number-of-times-a-driver-was-a-passenger.sql
+++ 2376-number-of-times-a-driver-was-a-passenger/number-of-times-a-driver-was-a-passenger.sql
@@ -1,6 +1,7 @@
-# Write your MySQL query statement below
-with unique_drivers as (
-    select distinct(driver_id) as ddi from rides
+WITH unique_drivers AS (
+    SELECT DISTINCT driver_id AS ddi FROM rides
 )
-select r1.ddi as driver_id, count(r2.passenger_id) as cnt from unique_drivers as r1 left join rides as r2 on r1.ddi = r2.passenger_id 
-group by r1.ddi;
+SELECT r1.ddi AS driver_id, COUNT(r2.passenger_id) AS cnt 
+FROM unique_drivers AS r1 
+LEFT JOIN rides AS r2 ON r1.ddi = r2.passenger_id 
+GROUP BY r1.ddi;
