3931b47604c54a6f633e510bf750ddfff3f00044
Ata Turhan <ataturhan21@gmail.com>
2024-03-11 21:20:39 -0600

    Time: 801 ms (81.85%) | Memory: 0B (100.00%) - LeetSync

--- 2441-arrange-table-by-gender/arrange-table-by-gender.sql
+++ 2441-arrange-table-by-gender/arrange-table-by-gender.sql
@@ -1,21 +1,12 @@
-WITH ranked AS (
-    SELECT 
-        user_id, 
-        gender, 
-        RANK() OVER (PARTITION BY gender ORDER BY user_id) + 
-        (CASE 
-            WHEN gender = 'female' THEN 0.1 
-            WHEN gender = 'other' THEN 0.2 
-            ELSE 0.3 
-        END) AS ranks 
-    FROM 
-        genders
-)
-
 SELECT 
     user_id, 
     gender 
 FROM 
-    ranked 
+    genders 
 ORDER BY 
-    ranks;
+    RANK() OVER (PARTITION BY gender ORDER BY user_id) + 
+        (CASE 
+            WHEN gender = 'female' THEN 0.1 
+            WHEN gender = 'other' THEN 0.2 
+            ELSE 0.3 
+        END);
