1346ab5608532ffaee0e455d1f56537d5e77074a
Ata Turhan <ataturhan21@gmail.com>
2024-03-15 19:46:12 -0600

    Time: 617 ms (63.73%) | Memory: 22.2 MB (70.11%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -4,12 +4,14 @@ class Solution:
         prefix_sum[0] = -1
         running_sum = 0
         max_len = 0
+
         for i, num in enumerate(nums):
             running_sum += 1 if num == 1 else -1
             if running_sum in prefix_sum:
                 max_len = max(max_len, i - prefix_sum[running_sum])
             else:
                 prefix_sum[running_sum] = i
+
         return max_len
                 
         
