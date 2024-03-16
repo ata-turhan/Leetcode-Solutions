823b368fcccdb5aeb2b8684b730fdbfe9793d6aa
Ata Turhan <ataturhan21@gmail.com>
2024-03-15 20:24:42 -0600

    Time: 138 ms (81.46%) | Memory: 18.3 MB (61.24%) - LeetSync

--- 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
@@ -1,10 +1,13 @@
 class Solution:
     def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x: (x[1], -x[0]))
+
         nums = []
+
         for start, end in intervals:
             if not nums or nums[-1] < start:
                 nums.extend([end - 1, end])
             elif nums[-2] < start:
                 nums.append(end)
+
         return len(nums)
