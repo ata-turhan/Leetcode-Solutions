b5bb247a402393b139f6f76d1d95da47c2b1b696
Ata Turhan <ataturhan21@gmail.com>
2024-03-22 17:23:26 -0500

    Time: 122 ms (79.91%) | Memory: 18.3 MB (6.43%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,6 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        zero_count = nums.count(0)
+        if zero_count == 0:
+            return
+            
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[k] = nums[i]
+                k += 1
+        nums[-zero_count:] = [0] * zero_count
+
+        """
         if 0 not in nums:
             return nums
         z = nums.index(0)
@@ -12,6 +24,7 @@ class Solution:
                 nums[i], nums[z] = nums[z], nums[i]
                 z += 1
             i += 1
+        """
 
 
         
