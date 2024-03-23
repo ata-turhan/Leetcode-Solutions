bc13d70a18d9c3a9fa6869e65c3bda53485b30c0
Ata Turhan <ataturhan21@gmail.com>
2024-03-23 15:24:25 -0500

    Time: 268 ms (92.10%) | Memory: 32.2 MB (62.21%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,12 +5,17 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # Check if the list is empty or has only one node, it's always a palindrome
         if not head or not head.next:
             return True
+        
+        # Find the middle of the linked list using the slow and fast pointers
         slow, fast = head, head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+        
+        # Reverse the second half of the linked list
         prev = None
         cur = slow
         while cur:
@@ -18,6 +23,8 @@ class Solution:
             cur.next = prev
             prev = cur
             cur = nxt
+        
+        # Compare values of nodes from both halves of the linked list
         start = head
         end = prev
         while start and end:
@@ -25,6 +32,7 @@ class Solution:
                 return False
             start = start.next
             end = end.next
-        return True
         
+        # If all values match, it's a palindrome
+        return True
         
