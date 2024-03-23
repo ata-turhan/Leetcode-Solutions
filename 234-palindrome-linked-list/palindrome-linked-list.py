2069034d0f56bd8979c8bb87b320ce41b3aeb905
Ata Turhan <ataturhan21@gmail.com>
2024-03-23 15:23:19 -0500

    Time: 289 ms (58.31%) | Memory: 31.9 MB (90.75%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,65 +5,26 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if not head.next:
+        if not head or not head.next:
             return True
-        
-        middle_node = None
-        prev_end_node = ListNode()
-        prev_end_node.next = head
-        slow_ptr = head
-        fast_ptr = head
-        
-        while fast_ptr and fast_ptr.next:
-            prev_end_node = slow_ptr
-            slow_ptr = slow_ptr.next
-            fast_ptr = fast_ptr.next.next 
-        
-        left_end = prev_end_node
-        
-        if fast_ptr:
-            middle_node = slow_ptr
-            right_start = slow_ptr.next
-            slow_ptr = slow_ptr.next
-        else:
-            right_start = slow_ptr      
-    
-        prev_end_node.next = None
-        prev_end_node = None
-        
-        while slow_ptr:
-            next_node = slow_ptr.next
-            slow_ptr.next = prev_end_node
-            prev_end_node = slow_ptr
-            slow_ptr = next_node
-        
-        tail_ptr = prev_end_node
-        curr_ptr = head
-        is_palindrome = True
-        
-        while curr_ptr:
-            if curr_ptr.val != tail_ptr.val:
-                is_palindrome = False
-                break
-            curr_ptr = curr_ptr.next
-            tail_ptr = tail_ptr.next
-
-        prev_ptr = None
-        curr_ptr = prev_end_node
-        
-        while curr_ptr:
-            next_node = curr_ptr.next
-            curr_ptr.next = prev_ptr
-            prev_ptr = curr_ptr
-            curr_ptr = next_node
-        
-        if middle_node:
-            left_end.next = middle_node
-            middle_node.next = right_start
-        else:
-            left_end.next = right_start
-        
-        return is_palindrome
-        
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        prev = None
+        cur = slow
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        start = head
+        end = prev
+        while start and end:
+            if start.val != end.val:
+                return False
+            start = start.next
+            end = end.next
+        return True
         
         
