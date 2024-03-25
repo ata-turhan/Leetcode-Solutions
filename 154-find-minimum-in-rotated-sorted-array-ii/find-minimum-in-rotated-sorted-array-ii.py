dcd411221053df35ab74aa58087c704bf3b6d73f
Ata Turhan <ataturhan21@gmail.com>
2024-03-25 17:34:06 -0500

    Time: 50 ms (70.69%) | Memory: 17.1 MB (41.60%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.py
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.py
@@ -2,14 +2,22 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
 
+        # Perform binary search until left pointer exceeds the right pointer.
         while left <= right:            
-            mid = left + (right-left) // 2
+            mid = left + (right - left) // 2
 
+            # Check if the middle element is greater than the rightmost element.
             if nums[mid] > nums[right]:
+                # If so, the minimum element must be in the right half.
                 left = mid + 1
             elif nums[mid] < nums[right]:
+                # If the middle element is less than the rightmost element,
+                # the minimum element must be in the left half.
                 right = mid 
             else:
+                # If the middle element is equal to the rightmost element,
+                # we decrease the search range by adjusting the right pointer.
                 right -= 1
 
+        # At the end of the loop, left pointer points to the minimum element.
         return nums[left]
