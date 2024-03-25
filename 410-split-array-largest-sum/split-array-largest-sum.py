0247a4fbf664f58a225b52599071711838a2ad72
Ata Turhan <ataturhan21@gmail.com>
2024-03-25 18:00:44 -0500

    Time: 42 ms (56.37%) | Memory: 16.8 MB (14.89%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.py
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -1,34 +1,28 @@
 class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
-        n = len(nums)
-        
-        # Create a prefix sum array of nums.
-        prefix_sum = [0] + list(itertools.accumulate(nums))
-        
-        @functools.lru_cache(None)
-        def get_min_largest_split_sum(curr_index: int, subarray_count: int):
-            # Base Case: If there is only one subarray left, then all of the remaining numbers
-            # must go in the current subarray. So return the sum of the remaining numbers.
-            if subarray_count == 1:
-                return prefix_sum[n] - prefix_sum[curr_index]
-        
-            # Otherwise, use the recurrence relation to determine the minimum largest subarray sum
-            # between curr_index and the end of the array with subarray_count subarrays remaining.
-            minimum_largest_split_sum = prefix_sum[n]
-            for i in range(curr_index, n - subarray_count + 1):
-                # Store the sum of the first subarray.
-                first_split_sum = prefix_sum[i + 1] - prefix_sum[curr_index]
+        def split(max_val, m):
+            cur_sum = 0
+            k = 1
+            for num in nums:
+                if cur_sum + num <= max_val:
+                    cur_sum += num
+                else:
+                    cur_sum = num
+                    k += 1
+                    if k > m:
+                        return float("inf")
+            return k
+                    
 
-                # Find the maximum subarray sum for the current first split.
-                largest_split_sum = max(first_split_sum, 
-                                        get_min_largest_split_sum(i + 1, subarray_count - 1))
-
-                # Find the minimum among all possible combinations.
-                minimum_largest_split_sum = min(minimum_largest_split_sum, largest_split_sum)
-
-                if first_split_sum >= minimum_largest_split_sum:
-                    break
-            
-            return minimum_largest_split_sum
-        
-        return get_min_largest_split_sum(0, m)
+        left = max(nums)
+        right = sum(nums)
+        min_sum = float("inf")
+        while left <= right:
+            mid = left + (right - left) // 2
+            subarray_count = split(mid, m)
+            if subarray_count <= m:
+                right = mid - 1
+                min_sum = min(min_sum, mid)
+            else:
+                left = mid + 1
+        return min_sum
