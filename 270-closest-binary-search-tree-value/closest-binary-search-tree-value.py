5af11a6323404d7d24d223918f2eb151ee5bc0da
Ata Turhan <ataturhan21@gmail.com>
2024-03-25 15:09:44 -0500

    Time: 34 ms (89.06%) | Memory: 18.2 MB (29.79%) - LeetSync

--- 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
+++ 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
@@ -1,32 +1,41 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
         def search(root, target, min_val, max_val):
             if not root:
-                return [-1, float("inf")]
+                return -1, float("inf")
+            
             if root.val == target:
-                return [root.val, 0]
-            cur = [root.val, abs(root.val-target)]
-            left, right = [-1, float("inf")], [-1, float("inf")]
+                return root.val, 0
+            
+            cur_val = root.val
+            cur_diff = abs(cur_val - target)
+            
+            left_val, left_diff = -1, float("inf")
+            right_val, right_diff = -1, float("inf")
+            
             if root.val < target <= max_val:
-                right = search(root.right, target, root.val, max_val)
+                right_val, right_diff = search(root.right, target, root.val, max_val)
+                
             if min_val <= target < root.val:
-                left = search(root.left, target, min_val, root.val)
-            if left[1] <= right[1]:
-                if left[1] <= cur[1]:
-                    return left
+                left_val, left_diff = search(root.left, target, min_val, root.val)
+                
+            if left_diff <= right_diff:
+                if left_diff <= cur_diff:
+                    return left_val, left_diff
                 else:
-                    return cur
+                    return cur_val, cur_diff
             else:
-                if cur[1] <= right[1]:
-                    return cur
+                if cur_diff <= right_diff:
+                    return cur_val, cur_diff
                 else:
-                    return right
-        return search(root, target, float("-inf"), float("inf"))[0]
-
+                    return right_val, right_diff
         
+        return search(root, target, float("-inf"), float("inf"))[0]
