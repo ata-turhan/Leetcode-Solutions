45c9bc37da1d96683a11f1af70089694ab20fa1f
Ata Turhan <ataturhan21@gmail.com>
2024-03-24 19:18:06 -0500

    Time: 3359 ms (5.57%) | Memory: 18 MB (48.82%) - LeetSync

--- 753-open-the-lock/open-the-lock.py
+++ 753-open-the-lock/open-the-lock.py
@@ -1,28 +1,44 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        if "0000" == target:
+        # Check if target is already at '0000'
+        if target == "0000":
             return 0
 
-        q = deque()
+        # Initialize the queue with the starting combination '0000'
+        queue = deque()
         if "0000" not in deadends:
-            q.append(("0000", 0))
+            queue.append(("0000", 0))
         
+        # Set to track visited combinations
         visited = set()
         visited.add("0000")
 
-        while q:
-            node, dist = q.popleft()
+        # Perform BFS
+        while queue:
+            node, dist = queue.popleft()
 
+            # Generate possible next combinations by turning each wheel
             for i in range(4):
+                # Rotate the wheel forward
                 new_node1 = node[:i] + str((int(node[i]) + 1) % 10) + node[i+1:]
-                new_node2 =  node[:i] + str((int(node[i]) - 1) % 10) + node[i+1:]
+                # Rotate the wheel backward
+                new_node2 = node[:i] + str((int(node[i]) - 1) % 10) + node[i+1:]
 
+                # Check if the target combination is reached
                 if new_node1 == target or new_node2 == target:
                     return dist + 1
+                
+                # Check if the next combination is not a deadend and not visited yet
                 if new_node1 not in deadends and new_node1 not in visited:
                     visited.add(new_node1)
-                    q.append((new_node1, dist + 1))
+                    queue.append((new_node1, dist + 1))
+                
                 if new_node2 not in deadends and new_node2 not in visited:
                     visited.add(new_node2)
-                    q.append((new_node2, dist + 1))
+                    queue.append((new_node2, dist + 1))
+        
+        # Target combination cannot be reached
         return -1
