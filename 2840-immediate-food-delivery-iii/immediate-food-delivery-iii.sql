0e636f0fc2c9b6c5dcf96d119f839717099c242d
Ata Turhan <ataturhan21@gmail.com>
2024-03-25 20:41:38 -0500

    Time: 1513 ms (19.04%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2840-immediate-food-delivery-iii/immediate-food-delivery-iii.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select order_date, round(100*sum(if(order_date=customer_pref_delivery_date, 1, 0))/count(delivery_id), 2) as immediate_percentage  from delivery group by order_date order by order_date;
