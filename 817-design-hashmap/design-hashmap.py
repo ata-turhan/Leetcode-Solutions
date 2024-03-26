6519ba4a7f51c7db16b652fb4631f44ab9ffd78c
Ata Turhan <ataturhan21@gmail.com>
2024-03-26 15:24:53 -0500

    Time: 241 ms (29.84%) | Memory: 42.1 MB (18.27%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -1,20 +1,21 @@
 class MyHashMap:
 
     def __init__(self):
+        # Initialize an array to represent the HashMap, initially all set to -1
         self.arr = [-1] * (10**6 + 1)
-        
 
     def put(self, key: int, value: int) -> None:
+        # Store the value at the index corresponding to the key
         self.arr[key] = value
 
-        
-
     def get(self, key: int) -> int:
+        # Return the value stored at the index corresponding to the key
         return self.arr[key]
-        
 
     def remove(self, key: int) -> None:
+        # Remove the value stored at the index corresponding to the key by setting it to -1
         self.arr[key] = -1
+
 
 
 # Your MyHashMap object will be instantiated and called as such:
