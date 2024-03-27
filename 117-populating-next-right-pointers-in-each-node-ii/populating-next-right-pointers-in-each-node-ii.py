7e5afc8a9de3fcd307cd66ecdda876f33f54c85e
Ata Turhan <ataturhan21@gmail.com>
2024-03-27 16:45:25 -0500

    Time: 43 ms (59.64%) | Memory: 17.5 MB (95.38%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -1,17 +1,27 @@
-class Solution:    
-    def connect(self, root: Optional['Node']) -> Optional['Node']:
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root: Optional[Node]) -> Optional[Node]:
+        # Define a depth-first search (DFS) function to connect nodes on the same level
         def dfs(node):
+            # Base case: if the node is None, return None
             if not node:
                 return None
-
+            
+            # Store the next node on the same level
             next_node = node.next
-            while next_node and next_node.next:
-                if next_node.left or next_node.right:
-                    break
-                else:
-                    next_node = next_node.next
-                    
-
+            
+            # Iterate to find the next non-null node on the same level
+            while next_node and not (next_node.left or next_node.right):
+                next_node = next_node.next
+                
+            # Connect the left and right child nodes
             if node.left and node.right:
                 node.left.next = node.right
                 if next_node:
@@ -20,9 +30,12 @@ class Solution:
                 node.left.next = next_node.left or next_node.right
             elif node.right and next_node:
                 node.right.next = next_node.left or next_node.right
+            
+            # Recursively call the DFS function on the right and left child nodes
             dfs(node.right)
             dfs(node.left)
+            
             return node
 
-
+        # Call the DFS function on the root node and return the root
         return dfs(root)
