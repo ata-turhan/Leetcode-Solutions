365d88ee2c405f52684a93e7a677a04b4003b940
Ata Turhan <ataturhan21@gmail.com>
2024-03-26 20:01:10 -0500

    Time: 517 ms (29.03%) | Memory: 19.3 MB (50.02%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.py
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.py
@@ -1,12 +1,22 @@
-class Solution(object):
+class Solution:
     def numSubarrayProductLessThanK(self, nums, k):
-        if k <= 1: return 0
-        prod = 1
-        ans = left = 0
+        if k <= 1:
+            return 0
+        
+        prod = 1  # Initialize product of elements in the current subarray
+        ans = 0   # Initialize the count of subarrays with product less than k
+        left = 0  # Initialize the left pointer
+        
+        # Iterate through the elements in the list
         for right, val in enumerate(nums):
-            prod *= val
+            prod *= val  # Update the product by multiplying with the current element
+            
+            # Shrink the window by moving the left pointer until the product is less than k
             while prod >= k:
                 prod /= nums[left]
                 left += 1
+            
+            # Count the subarrays ending at the current position
             ans += right - left + 1
-        return ans
+        
+        return ans  # Return the total count of subarrays with product less than k
