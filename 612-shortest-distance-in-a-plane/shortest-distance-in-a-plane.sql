942b1ef0ca559ccf2e69a91d3a103ca55e788146
Ata Turhan <ataturhan21@gmail.com>
2024-03-27 21:31:56 -0500

    Time: 416 ms (67.77%) | Memory: 0B (100.00%) - LeetSync

--- 612-shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
+++ 612-shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
@@ -1,2 +1,8 @@
-select min(round(power(power((p1.x-p2.x), 2) + power((p1.y-p2.y), 2), 0.5), 2)) as shortest 
- from point2d as p1 join point2d as p2 on p1.x != p2.x or p1.y != p2.y
+SELECT 
+    MIN(ROUND(POWER(POWER((p1.x - p2.x), 2) + POWER((p1.y - p2.y), 2), 0.5), 2)) AS shortest 
+FROM 
+    point2d AS p1
+JOIN 
+    point2d AS p2 
+ON 
+    (p1.x != p2.x OR p1.y != p2.y);
