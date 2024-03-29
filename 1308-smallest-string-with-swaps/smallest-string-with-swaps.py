36dcd008bacb4da40e75c6a216825dfe53bec4f9
Ata Turhan <ataturhan21@gmail.com>
2024-03-29 16:07:58 -0500

    Time: 486 ms (76.03%) | Memory: 52.9 MB (48.60%) - LeetSync

--- 1308-smallest-string-with-swaps/smallest-string-with-swaps.py
+++ 1308-smallest-string-with-swaps/smallest-string-with-swaps.py
@@ -1,23 +1,37 @@
 class UnionFind:
-    def __init__(self, n):
-        self.parent = [i for i in range(n)]
-        self.rank = [1] * n
+    def __init__(self, size):
+        # Initialize parent array with each element as its own root
+        self.parent = [i for i in range(size)]
+        # Initialize rank array to track the height of each tree
+        self.rank = [1] * size
+        # Initialize component count to the size initially
+        self.component_count = size
 
     def find(self, x):
+        # Find the root of the set containing element x
         if x != self.parent[x]:
+            # Path compression: set parent of x to the root of its set
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
 
     def union(self, x, y):
+        # Union two elements x and y
         x_root, y_root = self.find(x), self.find(y)
+
         if x_root != y_root:
+            # Union by rank: attach the smaller tree to the root of the larger tree
             if self.rank[x_root] > self.rank[y_root]:
                 self.parent[y_root] = x_root
-            elif self.rank[x_root] < self.rank[y_root]:
-                self.parent[x_root] = y_root
+                self.rank[x_root] += self.rank[y_root]
             else:
-                self.parent[y_root] = x_root
-                self.rank[x_root] += 1
+                self.parent[x_root] = y_root
+                self.rank[y_root] += self.rank[x_root]
+            # Decrease component count as two components are merged
+            self.component_count -= 1
+
+    def get_component_count(self):
+        # Get the current number of connected components
+        return self.component_count
 
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
