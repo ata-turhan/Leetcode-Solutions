bb618699614e334de410e660f96009ab2bec2644
Ata Turhan <ataturhan21@gmail.com>
2024-03-28 19:32:54 -0500

    Time: 854 ms (85.50%) | Memory: 30.9 MB (80.15%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -2,12 +2,17 @@ from collections import defaultdict
 
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        max_el = max(nums)
-        res = 0
-        max_idx = []
+        max_element = max(nums)  # Find the maximum element in the array
+        result = 0  # Initialize the result count
+        max_indices = []  # List to store the indices of the maximum element
+        
+        # Iterate through the array and track the indices of the maximum element
         for i, num in enumerate(nums):
-            if num == max_el:
-                max_idx.append(i)
-            if len(max_idx) >= k:
-                res += max_idx[-k] + 1
-        return res
+            if num == max_element:
+                max_indices.append(i)
+            # If the count of maximum element indices reaches or exceeds k
+            if len(max_indices) >= k:
+                # Increment the result count by the index of the (kth occurrence - k + 1) maximum element
+                result += max_indices[-k] + 1
+        
+        return result
