e4629c5a510a873e4c4dc0db0a545b984b4fe780
Ata Turhan <ataturhan21@gmail.com>
2024-04-03 21:45:39 -0500

    Time: 43 ms (7.95%) | Memory: 16.5 MB (38.30%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -1,12 +1,15 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
-        max_depth = 0
-        cur_depth = 0
-        for char in s:
-            if char == "(":
-                cur_depth += 1
-            elif char == ")":
-                cur_depth -= 1
-            max_depth = max(max_depth, cur_depth)
-        return max_depth
+        max_depth = 0  # Initialize the maximum depth
+        cur_depth = 0  # Initialize the current depth
         
+        # Iterate through each character in the string
+        for char in s:
+            if char == "(":  # If the character is an opening parenthesis
+                cur_depth += 1  # Increase the current depth
+            elif char == ")":  # If the character is a closing parenthesis
+                cur_depth -= 1  # Decrease the current depth
+            # Update the maximum depth encountered so far
+            max_depth = max(max_depth, cur_depth)
+        
+        return max_depth
