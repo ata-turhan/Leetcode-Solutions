2aed160c2b1096ce75d0454cbb2bb366574fc891
Ata Turhan <ataturhan21@gmail.com>
2024-04-04 19:40:16 -0500

    Time: 40 ms (43.60%) | Memory: 16.5 MB (46.83%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.py
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -1,13 +1,13 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        stack = []
+        stack = []  # Initialize a stack to store characters
         for char in s:
-            if not stack:
+            if not stack:  # If the stack is empty, append the current character
                 stack.append(char)
             else:
+                # Check if the current character and the top of the stack form a pair
                 if stack[-1].lower() != char.lower() or stack[-1] == char:
-                    stack.append(char)
+                    stack.append(char)  # If not, append the current character to the stack
                 else:
-                    stack.pop()
-        return "".join(stack)
-        
+                    stack.pop()  # If they form a pair, pop the character from the stack
+        return "".join(stack)  # Convert the stack to a string and return
