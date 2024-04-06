14bcd386b65edb24b744adf85a584432614a3bdf
Ata Turhan <ataturhan21@gmail.com>
2024-04-05 20:53:56 -0500

    Time: 1658 ms (51.68%) | Memory: 0B (100.00%) - LeetSync

---
+++ 580-count-student-number-in-departments/count-student-number-in-departments.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select dept_name, count(student_id) as student_number from department left join student using(dept_id) group by dept_name order by student_number desc, dept_name asc;
