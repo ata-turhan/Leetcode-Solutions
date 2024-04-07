a4a43dbd3a83e39b1a37fbf928999f598f9f546b
Ata Turhan <ataturhan21@gmail.com>
2024-04-07 09:13:53 -0500

    Time: 1413 ms (100.00%) | Memory: 67.9 MB (100.00%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.py
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.py
@@ -1,14 +1,16 @@
-class UnionFind():
+class UnionFind:
     def __init__(self, n):
-        self.root = [i for i in range(n)]
-        self.rank = [1] * n
-        self.weight = [2**31-1] * n
+        self.root = [i for i in range(n)]  # Initialize root array
+        self.rank = [1] * n  # Initialize rank array
+        self.weight = [2 ** 31 - 1] * n  # Initialize weight array
     
+    # Find operation with path compression
     def find(self, x):
         if x != self.root[x]:
             self.root[x] = self.find(self.root[x])
         return self.root[x]
 
+    # Union operation with union by rank
     def union(self, x, y, weight):
         x_root, y_root = self.find(x), self.find(y)
         if self.rank[x_root] > self.rank[y_root]:
@@ -22,19 +24,15 @@ class UnionFind():
 
 class Solution:
     def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
-        uf = UnionFind(n)
+        uf = UnionFind(n)  # Initialize UnionFind object
         for u, v, w in edges:
-            uf.union(u, v, w)
+            uf.union(u, v, w)  # Perform union operation on edges
         res = []
         for q in query:
             if q[0] == q[1]:
-                res.append(0)
-                print("ata")
+                res.append(0)  # Append 0 if source and destination are the same
             elif uf.find(q[0]) != uf.find(q[1]):
-                res.append(-1)
+                res.append(-1)  # Append -1 if source and destination are not in the same component
             else:
-                res.append(uf.weight[uf.find(q[0])])
+                res.append(uf.weight[uf.find(q[0])])  # Append weight of the component if source and destination are connected
         return res
-
-
-            
