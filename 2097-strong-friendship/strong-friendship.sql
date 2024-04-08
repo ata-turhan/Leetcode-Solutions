ff39fdbcf62134d7a11780754c64ba236126b374
Ata Turhan <ataturhan21@gmail.com>
2024-04-07 19:55:41 -0500

    Time: 762 ms (45.12%) | Memory: 0B (100.00%) - LeetSync

--- 2097-strong-friendship/strong-friendship.sql
+++ 2097-strong-friendship/strong-friendship.sql
@@ -1,9 +1,15 @@
-with all_pairs as (
-    select user1_id, user2_id from Friendship
-    union
-    select user2_id, user1_id from Friendship
+WITH all_pairs AS (
+    SELECT user1_id, user2_id FROM Friendship
+    UNION
+    SELECT user2_id, user1_id FROM Friendship
+),
+common_friends_count AS (
+    SELECT f1.user1_id, f1.user2_id, COUNT(f3.user2_id) AS common_friend 
+    FROM Friendship AS f1
+    JOIN all_pairs AS f2 ON f1.user1_id = f2.user1_id AND f1.user2_id != f2.user2_id 
+    JOIN all_pairs AS f3 ON f1.user2_id = f3.user1_id AND f2.user2_id = f3.user2_id 
+    GROUP BY f1.user1_id, f1.user2_id
 )
-
-
-    select f1.user1_id, f1.user2_id, count(f3.user2_id) as common_friend from Friendship as f1
-    join all_pairs as f2 on f1.user1_id = f2.user1_id and f1.user2_id != f2.user2_id join all_pairs as f3 on f1.user2_id = f3.user1_id and f2.user2_id = f3.user2_id group by f1.user1_id, f1.user2_id having common_friend >= 3; 
+SELECT user1_id, user2_id, common_friend 
+FROM common_friends_count 
+WHERE common_friend >= 3;
