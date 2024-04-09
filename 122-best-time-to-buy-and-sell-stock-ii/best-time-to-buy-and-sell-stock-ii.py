d87c5873837819025d2612684e6b17f46eeb9bfb
Ata Turhan <ataturhan21@gmail.com>
2024-04-08 21:49:13 -0500

    Time: 56 ms (67.75%) | Memory: 17.4 MB (93.51%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,21 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
+        profit = 0  # Initialize profit to 0
+        # Iterate through the prices
         for i in range(1, len(prices)):
-            profit += max(0, prices[i]-prices[i-1])
-        return profit 
-        
-        l = 0
-        r = 1
-        brought = prices[0]
-        profit = 0
-        while r < len(prices):
-            if prices[r] <= brought:
-                brought = prices[r]
-            else:
-                profit += prices[r] - brought
-                brought = prices[r]
-            r += 1
-        return profit
+            # Add the positive difference between consecutive prices to profit
+            profit += max(0, prices[i] - prices[i-1])
+        return profit  # Return the total profit
+
             
         
