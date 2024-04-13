9d0ca9b4c7b1704a1a64e3f4adb640ad879d195e
Ata Turhan <ataturhan21@gmail.com>
2024-04-13 11:30:47 -0500

    Time: 404 ms (60.86%) | Memory: 24.7 MB (60.05%) - LeetSync

--- 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.py
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.py
@@ -1,14 +1,14 @@
 class Solution:
     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
         def count_subarrays(nums: List[int], bound: int) -> int:
-            count = max_count = 0
+            count = total_count = 0
             for num in nums:
                 if num <= bound:
                     count += 1
                 else:
                     count = 0
-                max_count += count
-            return max_count
+                total_count += count
+            return total_count
         
         max_right = count_subarrays(nums, right)
         max_left = count_subarrays(nums, left - 1)
