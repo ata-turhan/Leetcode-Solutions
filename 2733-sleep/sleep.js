3bf307dfaaa58e35f7332fed4e12ee3bb101783a
Ata Turhan <ataturhan21@gmail.com>
2024-04-15 21:52:47 -0500

    Time: 51 ms (64.15%) | Memory: 48.5 MB (81.80%) - LeetSync

---
+++ 2733-sleep/sleep.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{resolve("success")}, millis);
+    })
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
