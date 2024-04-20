df1198f8b7b4825900a20faea91b7e5391afca84
Ata Turhan <ataturhan21@gmail.com>
2024-04-19 21:27:01 -0500

    Time: 52 ms (79.92%) | Memory: 49 MB (44.77%) - LeetSync

---
+++ 2762-cache-with-time-limit/cache-with-time-limit.js
@@ -0,0 +1,40 @@
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    const existedBefore = this.cache.has(key);
+    if (existedBefore)
+        clearTimeout(this.cache.get(key).timeoutId)
+    const timeoutId = setTimeout(()=>this.cache.delete(key), duration)
+    this.cache.set(key, {value, timeoutId})
+    return existedBefore
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    return this.cache.has(key) ? this.cache.get(key).value : -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
