087ac311254a464013be9e0c457c9ba3baf7a05c
Ata Turhan <ataturhan21@gmail.com>
2024-04-21 16:29:58 -0500

    Time: 39 ms (40.56%) | Memory: 16.5 MB (84.02%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,12 +1,27 @@
+from typing import List
+
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for i in range(len(digits)-1, -1, -1):
-            digits[i] += 1
-            if digits[i] == 10:
-                digits[i] = 0
+        """
+        Adds one to the given integer represented by an array of digits.
+
+        Args:
+        - digits: List of integers representing a non-negative integer
+
+        Returns:
+        - List of integers representing the result of adding one to the input number
+        """
+        # Iterate through the digits in reverse order
+        for i in range(len(digits) - 1, -1, -1):
+            digits[i] += 1  # Increment the current digit by one
+            if digits[i] == 10:  # If the digit becomes 10 after incrementing
+                digits[i] = 0  # Set the digit to 0
             else:
-                break
+                break  # Stop the loop if there is no carry-over
+
+        # If the most significant digit becomes 0 after incrementing (indicating a carry-over)
         if digits[0] == 0:
-            digits[0] = 1
-            digits.append(0)
-        return digits
+            digits[0] = 1  # Set the most significant digit to 1
+            digits.append(0)  # Append a new least significant digit (0)
+
+        return digits  # Return the modified list of digits representing the result
