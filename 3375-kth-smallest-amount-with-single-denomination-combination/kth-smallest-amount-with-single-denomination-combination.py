f724a3b528b858cbaf28b5c8e3130d87469755b6
Ata Turhan <ataturhan21@gmail.com>
2024-04-20 22:22:11 -0500

    Time: 187 ms (56.00%) | Memory: 17.2 MB (17.68%) - LeetSync

--- 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.py
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.py
@@ -4,11 +4,15 @@ from math import lcm
 
 class Solution:
     def findKthSmallest(self, coins: List[int], k: int) -> int:
+        # Get the length of the coins list
         n = len(coins)
+        
         # Store combinations of coins and their least common multiple
         combinations_lcm = defaultdict(list)
         for i in range(1, n + 1):
+            # Generate all combinations of coins of length i
             for comb in combinations(coins, i):
+                # Calculate the least common multiple of the current combination
                 combinations_lcm[len(comb)].append(lcm(*comb))
         
         # Function to count the number of multiples of least common multiples less than or equal to target
@@ -16,6 +20,7 @@ class Solution:
             ans = 0
             for i in range(1, n + 1):
                 for lcm_val in dic[i]:
+                    # Count the number of multiples of lcm_val less than or equal to target
                     ans += target // lcm_val * pow(-1, i + 1)
             return ans
         
@@ -23,10 +28,13 @@ class Solution:
         start, end = min(coins), min(coins) * k
         while start + 1 < end:
             mid = (start + end) // 2
+            # Check if the count of multiples less than or equal to mid is greater than or equal to k
             if count_multiples(combinations_lcm, mid) >= k:
                 end = mid
             else:
                 start = mid
+        
+        # Determine the kth smallest number by checking if count at start or end is greater than or equal to k
         if count_multiples(combinations_lcm, start) >= k:
             return start
         else:
