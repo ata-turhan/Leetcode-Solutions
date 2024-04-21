0a3e5b4cc5efc971f3d0baa849b10b3f0e1d9ebf
Ata Turhan <ataturhan21@gmail.com>
2024-04-21 13:34:29 -0500

    Time: 232 ms (100.00%) | Memory: 18.5 MB (100.00%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
@@ -4,7 +4,7 @@ class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
         # Initialize dictionaries to store the last index of each lowercase and corresponding uppercase letter encountered in the word
         last_small_letter_index = defaultdict(int)
-        last_large_letter_index = defaultdict(lambda: -1)
+        first_large_letter_index = defaultdict(lambda: -1)
         
         # Iterate through each character in the word
         for i, letter in enumerate(word):
@@ -14,15 +14,15 @@ class Solution:
                 last_small_letter_index[letter] = i
             else:
                 # Update the last index of the lowercase equivalent of the uppercase letter in the large_letters dictionary
-                if last_large_letter_index[letter.lower()] == -1:
-                    last_large_letter_index[letter.lower()] = i
+                if first_large_letter_index[letter.lower()] == -1:
+                    first_large_letter_index[letter.lower()] = i
         
         # Initialize a variable to count the number of special characters
         special_char_count = 0
         
         # Iterate through each lowercase letter and check if it has a corresponding uppercase letter with a smaller index
         for letter in last_small_letter_index:
-            if letter in last_large_letter_index and last_small_letter_index[letter] < last_large_letter_index[letter]:
+            if letter in first_large_letter_index and last_small_letter_index[letter] < first_large_letter_index[letter]:
                 special_char_count += 1
         
         # Return the count of special characters
