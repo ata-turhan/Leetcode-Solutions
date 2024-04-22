edf2375501c58f3c9750aaca3b726479e0df65f0
Ata Turhan <ataturhan21@gmail.com>
2024-04-21 20:09:39 -0500

    Time: 54 ms (98.99%) | Memory: 49.1 MB (96.35%) - LeetSync

---
+++ 2807-execute-asynchronous-functions-in-parallel/execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,33 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+        if (functions.length === 0) {
+            resolve([]); // Resolve with an empty array if no functions are provided
+        } else {
+            const res = new Array(functions.length);
+            let count = 0; // Counter to track resolved promises
+
+            functions.forEach((func, idx) => {
+                func().then(
+                    result => {
+                        res[idx] = result;
+                        count++; // Increment counter when promise resolves
+                        if (count === functions.length) {
+                            resolve(res); // Resolve with results array when all promises are resolved
+                        }
+                    }
+                ).catch(
+                    err => reject(err) // Reject if any promise rejects
+                );
+            });
+        }
+    });
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
