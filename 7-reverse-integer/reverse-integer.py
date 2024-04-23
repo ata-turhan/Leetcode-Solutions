e32130838b5f0a1f346e616aa323f57138c9017f
Ata Turhan <ataturhan21@gmail.com>
2024-04-22 21:09:16 -0500

    Time: 37 ms (51.29%) | Memory: 16.5 MB (68.89%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -9,23 +9,25 @@ class Solution:
         Returns:
         - Reversed integer or 0 if the result overflows
         """
-        INT_MAX = 2**31 - 1
-        
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+
         # Handle negative numbers
         sign = -1 if x < 0 else 1
         x = abs(x)
-        
+
         # Reverse the digits of the integer
         rev_x = 0
         while x != 0:
             digit = x % 10
             # Check for overflow before adding the next digit
-            if rev_x > (INT_MAX - digit) // 10:
+            if sign == 1 and rev_x > (INT_MAX - digit) // 10:
+                return 0
+            elif sign == -1 and rev_x > (-INT_MIN - digit) // 10:
                 return 0
             rev_x = rev_x * 10 + digit
             x //= 10
-        
+
         # Apply sign to the reversed integer
         rev_x *= sign
-        
+ 
         return rev_x
