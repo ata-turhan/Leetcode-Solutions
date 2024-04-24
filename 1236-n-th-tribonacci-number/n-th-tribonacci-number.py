34b30f0a014b0a6bdd475a77270446d2f5892070
Ata Turhan <ataturhan21@gmail.com>
2024-04-23 20:05:00 -0500

    Time: 41 ms (13.84%) | Memory: 16.6 MB (31.32%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,6 +1,18 @@
+from functools import cache
+
 class Solution:
     @cache
     def tribonacci(self, n: int) -> int:
+        """
+        Calculate the nth Tribonacci number using recursion and memoization.
+
+        Args:
+        - n: An integer representing the index of the Tribonacci number to calculate.
+
+        Returns:
+        - The nth Tribonacci number.
+        """
+        # Base cases
         if n == 0:
             return 0
         elif n == 1:
@@ -8,4 +20,5 @@ class Solution:
         elif n == 2:
             return 1
         else:
+            # Recursive calculation with memoization
             return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
