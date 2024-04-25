cb1cd51df2af36de200aed7bd432818388d897a8
Ata Turhan <ataturhan21@gmail.com>
2024-04-24 20:12:34 -0500

    Time: 1861 ms (22.97%) | Memory: 17.3 MB (93.33%) - LeetSync

---
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestIdealString(self, s: str, k: int) -> int:
+        dp = [0] * 26
+        res = 0
+        for i in range(len(s)):
+            cur_ord = ord(s[i]) - 97
+            new_val = 1
+            for j in range(26):
+                if abs(j - cur_ord) <= k:
+                    new_val = max(new_val, 1 + dp[j])
+            dp[cur_ord] = max(dp[cur_ord], new_val)
+            res = max(res, max(dp))
+        return res
+        
