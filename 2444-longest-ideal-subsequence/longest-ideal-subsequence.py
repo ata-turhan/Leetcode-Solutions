8e5e3e5140ea9a2f4f56400749e198cf0ae3d2bf
Ata Turhan <ataturhan21@gmail.com>
2024-04-24 20:13:04 -0500

    Time: 1605 ms (31.85%) | Memory: 17.3 MB (93.33%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.py
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.py
@@ -9,6 +9,5 @@ class Solution:
                 if abs(j - cur_ord) <= k:
                     new_val = max(new_val, 1 + dp[j])
             dp[cur_ord] = max(dp[cur_ord], new_val)
-            res = max(res, max(dp))
-        return res
+        return max(dp)
         
