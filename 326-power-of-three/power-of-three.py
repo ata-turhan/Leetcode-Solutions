8c1c87ea5905eb014d0ea931ea2349d590a38dd7
Ata Turhan <ataturhan21@gmail.com>
2024-04-27 20:01:25 -0500

    Time: 73 ms (37.36%) | Memory: 16.7 MB (23.49%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -4,14 +4,19 @@ class Solution:
             return True
         if n < 3:
             return False
+        
         num = 3
+        # Check if n is a power of 3 by continuously squaring num
         while num <= n:
             if num == n:
                 return True
-            num **= 2
+            num *= num
+        
+        # Check if n is a power of 3 by continuously taking the square root of num
         num = num**0.5
         while num <= n:
             if num == n:
                 return True
             num *= 3
+        
         return False
