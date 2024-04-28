290a7bd6aa344a897ee32ad81acfb396b311e86f
Ata Turhan <ataturhan21@gmail.com>
2024-04-27 19:45:04 -0500

    Time: 1278 ms (74.70%) | Memory: 55.2 MB (77.41%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -1,9 +1,17 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        arr = [True] * (n)
-        arr[0:2] = [False] * 2
-        for i in range(2, int(n**0.5)+1):
-            for j in range(i+i, n, i):
-                arr[j] = False
-        return sum(arr)
+        # Initialize an array to mark numbers as prime or composite
+        is_prime = [True] * n
+        # 0 and 1 are not prime
+        is_prime[0:2] = [False] * 2
         
+        # Iterate through numbers up to the square root of n
+        for i in range(2, int(n**0.5)+1):
+            # If i is prime, mark its multiples as composite
+            if is_prime[i]:
+                # Start from i*i and step by i, marking multiples as composite
+                for j in range(i*i, n, i):
+                    is_prime[j] = False
+        
+        # Count the number of primes
+        return sum(is_prime)
