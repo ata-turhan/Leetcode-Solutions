b8617dfac67619f451a25b75d74615b89cdcedfa
Ata Turhan <ataturhan21@gmail.com>
2024-04-29 11:50:01 -0500

    Time: 575 ms (83.12%) | Memory: 31 MB (50.32%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.py
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.py
@@ -1,15 +1,10 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         total = reduce(lambda a, b:a^b, nums)
-        total_bin = bin(total)[2:]
-        k_bin = bin(k)[2:]
+        res = total ^ k
         count = 0
-        while len(total_bin) > len(k_bin):
-            k_bin = "0" + k_bin
-        while len(total_bin) < len(k_bin):
-            total_bin = "0" + total_bin
-        for a, b in zip(total_bin, k_bin):
-            if a != b:
-                count += 1
+        while res > 0:
+            res &= res-1
+            count += 1
         return count
         
