c27b1287ded8658e1a173517f74aa4c5626dc72f
Ata Turhan <ataturhan21@gmail.com>
2024-05-01 19:36:20 -0500

    Time: 842 ms (92.24%) | Memory: 0B (100.00%) - LeetSync

--- 3283-find-third-transaction/find-third-transaction.sql
+++ 3283-find-third-transaction/find-third-transaction.sql
@@ -1,5 +1,20 @@
-# Write your MySQL query statement below
-with ranks_tx as (
-    select *, rank() over (partition by user_id order by transaction_date asc) as ranks from Transactions 
+WITH ranks_tx AS (
+    SELECT 
+        *,
+        RANK() OVER (PARTITION BY user_id ORDER BY transaction_date ASC) AS ranks
+    FROM 
+        Transactions
 )
-select r1.user_id, r1.spend as third_transaction_spend, r1.transaction_date as third_transaction_date   from ranks_tx as r1 left join ranks_tx as r2 on r1.ranks = 3 and r1.ranks - 1 = r2.ranks and r1.user_id = r2.user_id left join ranks_tx as r3 on r2.ranks - 1 = r3.ranks and  r1.user_id = r3.user_id where r1.spend > r2.spend and r1.spend > r3.spend
+SELECT 
+    r1.user_id,
+    r1.spend AS third_transaction_spend,
+    r1.transaction_date AS third_transaction_date
+FROM 
+    ranks_tx AS r1
+LEFT JOIN 
+    ranks_tx AS r2 ON r1.ranks = 3 AND r1.ranks - 1 = r2.ranks AND r1.user_id = r2.user_id
+LEFT JOIN 
+    ranks_tx AS r3 ON r2.ranks - 1 = r3.ranks AND r1.user_id = r3.user_id
+WHERE 
+    r1.spend > r2.spend 
+    AND r1.spend > r3.spend;
