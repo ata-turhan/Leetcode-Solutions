1edea7c44a40c41e9361293cda0658a87e6c57bd
Ata Turhan <ataturhan21@gmail.com>
2024-05-02 23:41:40 -0500

    Time: 30 ms (85.86%) | Memory: 16.5 MB (49.32%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -1,22 +1,27 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
+        # Split version strings into lists of integers
         v1 = list(map(int, version1.split(".")))
         v2 = list(map(int, version2.split(".")))
+        
+        # Initialize pointers for version parts
         i, j = 0, 0
+        
+        # Iterate through version parts
         while i < len(v1) or j < len(v2):
-            if i >= len(v1):
-                el1 = 0
-            else:
-                el1 = v1[i]
-            if j >= len(v2):
-                el2 = 0
-            else:
-                el2 = v2[j]
-            if el1 < el2:
+            # Get the current part for each version, or 0 if no more parts
+            part1 = v1[i] if i < len(v1) else 0
+            part2 = v2[j] if j < len(v2) else 0
+            
+            # Compare the parts
+            if part1 < part2:
                 return -1
-            elif el1 > el2:
+            elif part1 > part2:
                 return 1
             else:
+                # Move to the next part
                 i += 1
                 j += 1
+                
+        # Both versions are identical
         return 0
