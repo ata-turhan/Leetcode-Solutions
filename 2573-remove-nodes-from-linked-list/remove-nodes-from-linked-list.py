1452230ef3d473566f7e000859ac65f906495d46
Ata Turhan <ataturhan21@gmail.com>
2024-05-05 21:06:04 -0500

    Time: 440 ms (43.67%) | Memory: 51.5 MB (59.62%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -7,27 +7,27 @@ class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head.next:
             return head
+
+        # Reverse the linked list
         prev = None
         cur = head
         while cur:
-            nxt = cur.next
-            cur.next = prev
-            prev = cur
-            cur = nxt
+            cur.next, prev, cur = prev, cur, cur.next
         tail = prev
-        max_val = tail.val
-        cur = tail
+        
+        # Find the maximum value to the right and remove nodes with values smaller than max_val in the reversed list
+        cur = prev
+        max_val = float('-inf')  # Initialize max_val to negative infinity
         while cur:
             max_val = max(max_val, cur.val)
             while cur.next and cur.next.val < max_val:
                 cur.next = cur.next.next
             cur = cur.next
+
+        # Reverse the modified linked list back to its original order
         prev = None
         cur = tail
         while cur:
-            nxt = cur.next
-            cur.next = prev
-            prev = cur
-            cur = nxt
+            cur.next, prev, cur = prev, cur, cur.next
         return prev
         
