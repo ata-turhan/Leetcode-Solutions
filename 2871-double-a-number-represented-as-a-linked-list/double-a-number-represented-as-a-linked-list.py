741ac2be3e0fbc7aa0dc2c8180b5975bd4e43a6d
Ata Turhan <ataturhan21@gmail.com>
2024-05-06 20:46:56 -0500

    Time: 230 ms (72.46%) | Memory: 19.3 MB (86.07%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
@@ -1,20 +1,20 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Reverse the linked list
         prev, cur = None, head
         while cur:
             cur.next, prev, cur = prev, cur, cur.next
         cur = prev
+        
+        # Traverse the reversed list and double the digits while considering carry
         carry = 0
         while cur.next:
             new_val = cur.val * 2 + carry
             carry = new_val // 10
             cur.val = new_val % 10
             cur = cur.next
+        
+        # Process the last node separately to handle any remaining carry
         new_val = cur.val * 2 + carry
         cur.val = new_val % 10
         carry = new_val // 10
@@ -22,10 +22,11 @@ class Solution:
             new_node = ListNode(val=carry)
             cur.next = new_node
             cur = cur.next
+        
+        # Re-reverse the list to restore the original order
         head = cur
         prev, cur = None, prev
         while cur:
             cur.next, prev, cur = prev, cur, cur.next
-        return head
-
         
+        return head
