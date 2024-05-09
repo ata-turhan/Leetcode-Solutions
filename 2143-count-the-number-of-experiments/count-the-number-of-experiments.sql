9c06d01da232116271e30e31ad9f276fc6f0c3ce
Ata Turhan <ataturhan21@gmail.com>
2024-05-08 20:04:12 -0500

    Time: 914 ms (89.82%) | Memory: 0B (100.00%) - LeetSync

--- 2143-count-the-number-of-experiments/count-the-number-of-experiments.sql
+++ 2143-count-the-number-of-experiments/count-the-number-of-experiments.sql
@@ -1,22 +1,44 @@
-with platforms as (
-    select "Android" as platform
-    union
-    select "IOS" as platform
-    union
-    select "Web" as platform
+-- Define platforms
+WITH platforms AS (
+    SELECT 'Android' AS platform
+    UNION
+    SELECT 'IOS' AS platform
+    UNION
+    SELECT 'Web' AS platform
 ),
-experiment_names as (
-    select "Reading" as experiment_name 
-    union
-    select "Sports" as experiment_name 
-    union
-    select "Programming" as experiment_name 
+-- Define experiment names
+experiment_names AS (
+    SELECT 'Reading' AS experiment_name 
+    UNION
+    SELECT 'Sports' AS experiment_name 
+    UNION
+    SELECT 'Programming' AS experiment_name 
 ),
-platforms_and_experiment_names as (
-    select * from platforms, experiment_names
+-- Combine platforms and experiment names
+platforms_and_experiment_names AS (
+    SELECT * 
+    FROM platforms
+    CROSS JOIN experiment_names
 ),
-grpd_experiments as (
-    select platform , experiment_name, count(experiment_id) as num_experiments from Experiments group by platform, experiment_name
+-- Count the number of experiments for each platform-experiment combination
+grpd_experiments AS (
+    SELECT 
+        platform, 
+        experiment_name, 
+        COUNT(experiment_id) AS num_experiments 
+    FROM 
+        Experiments 
+    GROUP BY 
+        platform, 
+        experiment_name
 )
-
-select platforms_and_experiment_names.platform, platforms_and_experiment_names.experiment_name, ifnull(grpd_experiments.num_experiments , 0) as num_experiments from platforms_and_experiment_names left join grpd_experiments using(platform, experiment_name)
+-- Select and display the results
+SELECT 
+    pen.platform, 
+    pen.experiment_name, 
+    COALESCE(ge.num_experiments, 0) AS num_experiments 
+FROM 
+    platforms_and_experiment_names pen
+LEFT JOIN 
+    grpd_experiments ge 
+    ON pen.platform = ge.platform AND pen.experiment_name = ge.experiment_name;
