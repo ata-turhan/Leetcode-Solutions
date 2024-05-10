2f3ca7f6225cb7b218e7160b945c7327dbc312a2
Ata Turhan <ataturhan21@gmail.com>
2024-05-09 20:07:24 -0500

    Time: 592 ms (66.44%) | Memory: 16.9 MB (78.98%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
@@ -1,15 +1,22 @@
+import heapq
+from typing import List
+
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
         heap = []
 
+        # Push fractions (arr[i]/arr[-1]) into the heap for all i
         for i in range(len(arr)-1):
             heapq.heappush(heap, (arr[i]/arr[len(arr)-1], i, len(arr)-1))
 
+        # Pop the smallest fraction from the heap (k-1) times
         for _ in range(k-1):
             smallest_frac, nominator, denominator = heapq.heappop(heap)
+            # If the nominator is less than the denominator - 1, push the next smaller fraction
             if nominator < denominator - 1:
                 heapq.heappush(heap, (arr[nominator]/arr[denominator-1], nominator, denominator-1))
 
+        # Get the kth smallest fraction from the heap
         kth_smallest_frac, kth_smallest_nominator, kth_smallest_denominator = heapq.heappop(heap)
+        # Return the kth smallest fraction as a list [nominator, denominator]
         return [arr[kth_smallest_nominator], arr[kth_smallest_denominator]]
-        
