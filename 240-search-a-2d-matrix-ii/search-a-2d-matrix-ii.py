8ceb087ab32c835ab78e690648763ec8e7c1d460
Ata Turhan <ataturhan21@gmail.com>
2024-05-20 15:00:40 -0500

    Time: 134 ms (80.93%) | Memory: 22.9 MB (74.13%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # an empty matrix obviously does not contain `target` (make this check
+        # because we want to cache `width` for efficiency's sake)
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+
+        # cache these, as they won't change.
+        height = len(matrix)
+        width = len(matrix[0])
+
+        # start our "pointer" in the bottom-left
+        row = height - 1
+        col = 0
+
+        while col < width and row >= 0:
+            if matrix[row][col] > target:
+                row -= 1
+            elif matrix[row][col] < target:
+                col += 1
+            else: # found it
+                return True
+        
+        return False
