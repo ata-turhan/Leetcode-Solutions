ddce465c5d2461df14d814756f5250caa16668f4
Ata Turhan <ataturhan21@gmail.com>
2024-05-21 19:45:48 -0500

    Time: 766 ms (70.83%) | Memory: 17 MB (61.63%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -9,11 +9,15 @@ class Solution:
         dp = [amount + 1] * (amount + 1)
         dp[0] = 0  # Base case: 0 coins are needed to make amount 0
 
+        coins.sort()
+
         # Iterate over each amount from 1 to the target amount
         for i in range(1, amount + 1):
             for coin in coins:
                 if i - coin >= 0:
                     dp[i] = min(dp[i], dp[i - coin] + 1)
+                else:
+                    break
         
         # If dp[amount] is still amount + 1, it means it's not possible to form the amount with the given coins
         return dp[amount] if dp[amount] != amount + 1 else -1
