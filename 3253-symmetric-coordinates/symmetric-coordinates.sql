43cc81c4bf328dea71676f558d1912b53cb165bb
Ata Turhan <ataturhan21@gmail.com>
2024-05-21 20:20:44 -0500

    Time: 685 ms (22.28%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3253-symmetric-coordinates/symmetric-coordinates.sql
@@ -0,0 +1,18 @@
+WITH coordinates_with_rn AS (
+    SELECT x, y, ROW_NUMBER() OVER (ORDER BY x, y) AS rn
+    FROM Coordinates
+)
+
+-- Main query to find symmetric pairs and ensure x <= y
+SELECT DISTINCT
+    c1.x, c1.y
+FROM 
+    coordinates_with_rn AS c1
+JOIN 
+    coordinates_with_rn AS c2
+ON 
+    c1.x = c2.y AND c1.y = c2.x AND c1.rn != c2.rn
+WHERE 
+    c1.x <= c1.y
+ORDER BY 
+    c1.x, c1.y;
