0c41979c46ca21dbe865b444f63ed55fb7eae004
Ata Turhan <ataturhan21@gmail.com>
2024-05-24 21:12:14 -0500

    Time: 37 ms (42.81%) | Memory: 16.5 MB (87.62%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -1,24 +1,35 @@
+from typing import List
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        # Initialize the result list and convert wordDict to a set for O(1) lookups
         res = []
         wordDict = set(wordDict)
 
         def backtrack(i, j, array):
-            if j == len(s)-1:
-                if s[i:j+1] in wordDict:
-                    array.append(s[i:j+1])
+            # Base case: If we have reached the end of the string
+            if j == len(s) - 1:
+                # Check if the current substring is in the wordDict
+                if s[i:j + 1] in wordDict:
+                    array.append(s[i:j + 1])
                     res.append(" ".join(array))
                     array.pop()
                 return
-            if s[i:j+1] in wordDict:
-                array.append(s[i:j+1])
-                backtrack(j+1, j+1, array)
-                array.pop()
-                backtrack(i, j+1, array)
-            else:
-                backtrack(i, j+1, array)
 
+            # If the current substring is in the wordDict, proceed with backtracking
+            if s[i:j + 1] in wordDict:
+                array.append(s[i:j + 1])
+                backtrack(j + 1, j + 1, array)
+                array.pop()
+                backtrack(i, j + 1, array)
+            else:
+                backtrack(i, j + 1, array)
+
+        # Start the backtracking process
         backtrack(0, 0, [])
         return res
 
-            
+# Example usage:
+# sol = Solution()
+# print(sol.wordBreak("catsanddog", ["cat", "cats", "and", "sand", "dog"]))  
+# Output: ["cats and dog", "cat sand dog"]
