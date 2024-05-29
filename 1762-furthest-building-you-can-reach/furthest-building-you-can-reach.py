1ca52d703f7409a6f011c841c2b90fd33735f22a
Ata Turhan <ataturhan21@gmail.com>
2024-05-29 14:57:11 -0500

    Time: 403 ms (95.73%) | Memory: 30.9 MB (96.74%) - LeetSync

---
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.py
@@ -0,0 +1,20 @@
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        ladder_allocations = [] # We'll use heapq to treat this as a min-heap.
+        for i in range(len(heights) - 1):
+            climb = heights[i + 1] - heights[i]
+            # If this is actually a "jump down", skip it.
+            if climb <= 0:
+                continue
+            # Otherwise, allocate a ladder for this climb.
+            heapq.heappush(ladder_allocations, climb)
+            # If we haven't gone over the number of ladders, nothing else to do.
+            if len(ladder_allocations) <= ladders:
+                continue
+            # Otherwise, we will need to take a climb out of ladder_allocations
+            bricks -= heapq.heappop(ladder_allocations)
+            # If this caused bricks to go negative, we can't get to i + 1
+            if bricks < 0:
+                return i
+        # If we got to here, this means we had enough to cover every climb.
+        return len(heights) - 1
