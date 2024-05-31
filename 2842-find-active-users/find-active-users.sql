819850d7492dea85365bdf51332b4eb50aae844b
Ata Turhan <ataturhan21@gmail.com>
2024-05-30 20:16:38 -0500

    Time: 1661 ms (77.70%) | Memory: 0B (100.00%) - LeetSync

--- 2842-find-active-users/find-active-users.sql
+++ 2842-find-active-users/find-active-users.sql
@@ -1,8 +1,32 @@
-with users_dates as (
-    select user_id, created_at, row_number() over (partition by user_id order by created_at asc) as rn from users
+-- CTE to assign row numbers to each user's creation dates, ordered by the creation date
+WITH users_dates AS (
+    SELECT 
+        user_id, 
+        created_at, 
+        ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY created_at ASC) AS rn
+    FROM 
+        users
 ),
-users_dates_next_dates as (
-    select us1.user_id, us1.created_at as prev_date, us2.created_at as next_date from users_dates as us1 join users_dates as us2 on us1.user_id = us2.user_id and us1.rn + 1 = us2.rn
+
+-- CTE to pair each user's creation dates with the subsequent creation date
+users_dates_next_dates AS (
+    SELECT 
+        us1.user_id, 
+        us1.created_at AS prev_date, 
+        us2.created_at AS next_date
+    FROM 
+        users_dates AS us1
+    JOIN 
+        users_dates AS us2 
+    ON 
+        us1.user_id = us2.user_id 
+        AND us1.rn + 1 = us2.rn
 )
 
-select distinct user_id from users_dates_next_dates where datediff(next_date, prev_date) <= 7;
+-- Main query to find users with consecutive creation dates within 7 days
+SELECT 
+    DISTINCT user_id 
+FROM 
+    users_dates_next_dates 
+WHERE 
+    DATEDIFF(next_date, prev_date) <= 7;
