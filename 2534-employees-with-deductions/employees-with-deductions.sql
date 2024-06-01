19ddf12a03a981abd43be04d5bf20af878a2ec23
Ata Turhan <ataturhan21@gmail.com>
2024-05-31 22:31:18 -0500

    Time: 1982 ms (42.19%) | Memory: 0B (100.00%) - LeetSync

--- 2534-employees-with-deductions/employees-with-deductions.sql
+++ 2534-employees-with-deductions/employees-with-deductions.sql
@@ -1,4 +1,21 @@
-with total_work_hours as (
-    select employee_id, sum(TIME_TO_SEC(TIMEDIFF(out_time, in_time))/60 + if(TIME_TO_SEC(TIMEDIFF(out_time, in_time))%60 = 0, 0, 1))/60 as total_hours from logs group by employee_id
+-- CTE to calculate the total work hours for each employee
+WITH total_work_hours AS (
+    SELECT 
+        employee_id, 
+        -- Calculate total work hours by summing the difference between out_time and in_time, rounded up to the nearest minute
+        SUM(TIME_TO_SEC(TIMEDIFF(out_time, in_time)) / 3600 + IF(TIME_TO_SEC(TIMEDIFF(out_time, in_time)) % 60 = 0, 0, 1) / 60) AS total_hours
+    FROM 
+        logs
+    GROUP BY 
+        employee_id
 )
-select employee_id from employees as e left join total_work_hours using(employee_id) where needed_hours > ifnull(total_hours, 0);
+
+-- Main query to find employees who have not met their needed hours
+SELECT 
+    e.employee_id
+FROM 
+    employees AS e
+LEFT JOIN 
+    total_work_hours USING (employee_id)
+WHERE 
+    e.needed_hours > IFNULL(total_hours, 0);
