e702e9a6113352a61ac93d152af7ac8edef2ec2f
Ata Turhan <ataturhan21@gmail.com>
2024-06-08 14:44:24 -0500

    Time: 1124 ms (15.07%) | Memory: 175.5 MB (14.56%) - LeetSync

--- 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.py
+++ 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.py
@@ -2,21 +2,16 @@ class Solution:
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
         @cache
         def dp(left, right):
-            i = left + n - 1 - right
+            i = left + n - 1 - right  # Calculate the index for the current multiplier
 
-            if i == m:
+            if i == m:  # Base case: all multipliers are used
                 return 0
 
+            # Calculate the maximum score by either taking the left or the right end
             return max(
-                multipliers[i] * nums[left] + dp(left+1, right),
-                multipliers[i] * nums[right] + dp(left, right-1),
-
+                multipliers[i] * nums[left] + dp(left + 1, right),
+                multipliers[i] * nums[right] + dp(left, right - 1)
             )
 
-
-
-
-
         n, m = len(nums), len(multipliers)
-        return dp(0, len(nums)-1)
-        
+        return dp(0, len(nums) - 1)  # Start with the full range of nums
