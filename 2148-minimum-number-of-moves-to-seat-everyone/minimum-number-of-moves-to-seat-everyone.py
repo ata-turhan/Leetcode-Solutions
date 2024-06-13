794a82aa7aabd3d70554b27a37eb35de62cd249c
Ata Turhan <ataturhan21@gmail.com>
2024-06-12 19:40:24 -0500

    Time: 64 ms (27.12%) | Memory: 16.7 MB (9.46%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
@@ -1,11 +1,14 @@
+from typing import List
+
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        seats.sort()
-        students.sort()
+        seats.sort()  # Sort the seats array
+        students.sort()  # Sort the students array
 
         moves = 0
 
-        for se, st in zip(seats, students):
-            moves += abs(se-st)
+        # Calculate the total moves required by matching each student to the closest seat
+        for seat, student in zip(seats, students):
+            moves += abs(seat - student)
 
-        return moves        
+        return moves
