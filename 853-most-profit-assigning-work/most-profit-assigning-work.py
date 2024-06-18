a26ef4bd6a91fe5412f3cc6fa255f5eb63023f60
Ata Turhan <ataturhan21@gmail.com>
2024-06-18 07:44:56 -0500

    Time: 345 ms (28.89%) | Memory: 19.8 MB (33.39%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.py
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -1,23 +1,32 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        # Create a list of tuples containing negative profit and its index
         profit_indices = [(-p, i) for i, p in enumerate(profit)]
+        
+        # Convert the list into a heap (min-heap based on negative profit)
         heapify(profit_indices)
+        
+        # Sort the worker list to assign them to the most profitable job they can do
         worker.sort()
-        wi = len(worker) - 1
+        worker_index = len(worker) - 1
         total_profit = 0
-        max_profit, mpi = heappop(profit_indices)
+        
+        # Get the maximum profit and its corresponding index
+        max_profit, max_profit_index = heappop(profit_indices)
         max_profit = -max_profit
-        while wi >= 0:
-            if worker[wi] >= difficulty[mpi]:
+        
+        # Iterate over workers in reverse order (from highest to lowest capability)
+        while worker_index >= 0:
+            # Check if the current worker can perform the job with the maximum profit
+            if worker[worker_index] >= difficulty[max_profit_index]:
                 total_profit += max_profit
-                wi -= 1
+                worker_index -= 1
             else:
+                # If not, get the next highest profit job
                 if profit_indices:
-                    max_profit, mpi = heappop(profit_indices)
+                    max_profit, max_profit_index = heappop(profit_indices)
                     max_profit = -max_profit
                 else:
                     break
-        return total_profit
-
-                
         
+        return total_profit
