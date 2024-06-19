b4f5588c8628a7961ad006cb019ad812ad34575b
Ata Turhan <ataturhan21@gmail.com>
2024-06-19 09:53:25 -0500

    Time: 47 ms (17.66%) | Memory: 16.6 MB (84.98%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -9,8 +9,6 @@ class Solution:
         
         while queue:
             start = queue.popleft()
-            if start == len(s):  # If we reach the end of the string, return True
-                return True
             
             for end in range(start + 1, len(s) + 1):
                 if end in seen:  # Skip if end index is already seen
@@ -19,5 +17,7 @@ class Solution:
                 if s[start:end] in words:  # If substring is in wordDict
                     queue.append(end)
                     seen.add(end)  # Mark end index as seen
+                    if end == len(s):  # If we reach the end of the string, return True
+                        return True
                 
         return False  # Return False if no valid segmentation is found
