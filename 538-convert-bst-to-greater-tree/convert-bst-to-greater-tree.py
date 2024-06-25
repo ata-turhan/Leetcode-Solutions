f8a2f2f17c33cd3876463fe4289d70866f7e5ea7
Ata Turhan <ataturhan21@gmail.com>
2024-06-24 22:18:02 -0500

    Time: 60 ms (51.86%) | Memory: 18.5 MB (76.41%) - LeetSync

---
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def inverse_inorder(node, current_sum):
+            if not node:
+                return current_sum
+            # Traverse the right subtree and update the current sum
+            right_sum = inverse_inorder(node.right, current_sum)
+            # Update the node value with the sum of greater values
+            node.val += right_sum
+            # Traverse the left subtree and update the current sum
+            left_sum = inverse_inorder(node.left, node.val)
+            return left_sum
+        
+        # Start the inverse inorder traversal with the initial sum as 0
+        inverse_inorder(root, 0)
+        return root
