d2d3bdaf1ab0598569e0d1af3bcf8a7f2e67d9d7
Ata Turhan <ataturhan21@gmail.com>
2024-06-28 06:16:01 +0300

    Time: 1274 ms (43.03%) | Memory: 45 MB (15.16%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
@@ -1,19 +1,24 @@
+from collections import defaultdict
+from typing import List
+
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
+        # Count the number of edges connected to each node
         edge_counts = defaultdict(int)
-
         for road in roads:
             edge_counts[road[0]] += 1
             edge_counts[road[1]] += 1
 
+        # List of nodes sorted by their edge counts
         nodes = list(range(n))
         nodes.sort(key=lambda node: edge_counts[node])
 
-        orders = list(range(1, n+1))
+        # Assign increasing importance values to nodes
+        importance_values = list(range(1, n + 1))
 
+        # Calculate the total importance based on the edge counts
         total_importance = 0
+        for importance, node in zip(importance_values, nodes):
+            total_importance += edge_counts[node] * importance
 
-        for order, node in zip(orders, nodes):
-            total_importance += edge_counts[node] * order
-
-        return total_importance        
+        return total_importance
