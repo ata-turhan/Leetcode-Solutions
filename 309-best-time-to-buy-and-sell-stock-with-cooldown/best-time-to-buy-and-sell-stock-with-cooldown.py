93fd6030f5f40c8bc225b3730e972d6cc7cf2372
Ata Turhan <ataturhan21@gmail.com>
2024-06-30 02:28:14 +0300

    Time: 52 ms (22.15%) | Memory: 21.6 MB (5.97%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,34 @@
+from typing import List
+from functools import cache
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Dictionary to store the results of subproblems
+        dp = {}  # key=(i, buying) val=max_profit
+
+        @cache
+        def dfs(i: int, buying: bool) -> int:
+            # Base case: If we've gone through all the prices
+            if i >= len(prices):
+                return 0
+            
+            # If the result for this subproblem is already computed, return it
+            if (i, buying) in dp:
+                return dp[(i, buying)]
+            
+            # Calculate the profit for the cooldown state
+            cooldown = dfs(i + 1, buying)
+            
+            if buying:
+                # If buying, calculate the profit if we buy at the current price
+                buy = dfs(i + 1, not buying) - prices[i]
+                dp[(i, buying)] = max(buy, cooldown)
+            else:
+                # If selling, calculate the profit if we sell at the current price
+                sell = dfs(i + 2, not buying) + prices[i]
+                dp[(i, buying)] = max(sell, cooldown)
+            
+            return dp[(i, buying)]
+        
+        # Start the DFS from the first day with the intention to buy
+        return dfs(0, True)
