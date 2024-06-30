b5596f7a4a9dd3c932e77f51f3ecf9a574867eb9
Ata Turhan <ataturhan21@gmail.com>
2024-06-30 18:47:36 +0300

    Time: 219 ms (78.45%) | Memory: 22.2 MB (28.08%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
@@ -3,7 +3,7 @@ class Solution:
         MODULO = 10**9 + 7
 
         @cache
-        def dfs(remaining_dice, num_sides, current_sum):
+        def dfs(remaining_dice, current_sum):
             # Base case: if the current sum equals the target and no dice are left
             if current_sum == target and remaining_dice == 0:
                 return 1
@@ -13,9 +13,9 @@ class Solution:
 
             total_ways = 0
             # Recursive case: explore each possible outcome of rolling a die
-            for side in range(1, num_sides + 1):
-                total_ways = (total_ways + dfs(remaining_dice - 1, num_sides, current_sum + side)) % MODULO
+            for side in range(1, k + 1):
+                total_ways = (total_ways + dfs(remaining_dice - 1, current_sum + side)) % MODULO
             return total_ways
 
         # Start the recursive search with all dice, the number of sides, and a current sum of 0
-        return dfs(n, k, 0)
+        return dfs(n, 0)
