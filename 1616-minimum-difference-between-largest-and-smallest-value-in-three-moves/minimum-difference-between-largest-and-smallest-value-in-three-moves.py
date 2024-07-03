54034068255983cf775b29d7470a11192b1ab69b
Ata Turhan <ataturhan21@gmail.com>
2024-07-03 03:36:08 +0300

    Time: 237 ms (99.29%) | Memory: 27.3 MB (92.92%) - LeetSync

---
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -0,0 +1,24 @@
+from typing import List
+import heapq
+
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        nums_size = len(nums)
+        
+        # If there are 4 or fewer elements, we can make the array equal with 0 moves
+        if nums_size <= 4:
+            return 0
+        
+        # Find the smallest four elements
+        smallest_four = sorted(heapq.nsmallest(4, nums))
+        
+        # Find the largest four elements
+        largest_four = sorted(heapq.nlargest(4, nums))
+        
+        min_diff = float("inf")
+        
+        # Compare the differences between the largest and smallest elements
+        for i in range(4):
+            min_diff = min(min_diff, largest_four[i] - smallest_four[i])
+        
+        return min_diff
