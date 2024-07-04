5d9312a80b5a46d0e9bc5263a425dfdf487fb2fe
Ata Turhan <ataturhan21@gmail.com>
2024-07-04 03:32:00 +0300

    Time: 417 ms (67.89%) | Memory: 16.7 MB (88.84%) - LeetSync

--- 454-4sum-ii/4sum-ii.py
+++ 454-4sum-ii/4sum-ii.py
@@ -1,13 +1,22 @@
+from collections import defaultdict
+from typing import List
+
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        first_sums = defaultdict(int)
+        sum_counts = defaultdict(int)
+        
+        # Compute all possible sums of pairs from nums1 and nums2
         for num1 in nums1:
             for num2 in nums2:
-                first_sums[num1 + num2] += 1
+                sum_counts[num1 + num2] += 1
         
-        res = 0
+        result = 0
+        
+        # Check pairs from nums3 and nums4 to find the negation in sum_counts
         for num3 in nums3:
             for num4 in nums4:
-                if -(num3 + num4) in first_sums:
-                    res += first_sums[-(num3 + num4)]
-        return res
+                target_sum = -(num3 + num4)
+                if target_sum in sum_counts:
+                    result += sum_counts[target_sum]
+        
+        return result
