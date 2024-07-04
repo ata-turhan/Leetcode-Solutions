680a558986c225acecd0f847132b790e7a12c154
Ata Turhan <ataturhan21@gmail.com>
2024-07-04 03:12:22 +0300

    Time: 710 ms (93.62%) | Memory: 53.4 MB (91.57%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.py
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.py
@@ -3,19 +3,30 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        fill_node = head
-        traverse_node = head.next 
-        while True:
-            while traverse_node.val != 0:
-                fill_node.val += traverse_node.val
-                traverse_node = traverse_node.next 
-            traverse_node = traverse_node.next
-            if not traverse_node:
+        current_fill_node = head  # Node to accumulate sum values
+        current_traverse_node = head.next  # Node to traverse the list
+        
+        while current_traverse_node:
+            # Sum the values until we encounter a zero
+            while current_traverse_node.val != 0:
+                current_fill_node.val += current_traverse_node.val
+                current_traverse_node = current_traverse_node.next 
+            
+            # Move to the next segment after zero
+            current_traverse_node = current_traverse_node.next
+            
+            # If end of the list is reached, break the loop
+            if not current_traverse_node:
                 break
-            else:
-                fill_node = fill_node.next 
-                fill_node.val = 0
-        fill_node.next = None
+            
+            # Move the fill node to the next position and reset its value to zero
+            current_fill_node = current_fill_node.next 
+            current_fill_node.val = 0
+        
+        # Remove all nodes after the last filled node
+        current_fill_node.next = None
+        
         return head
