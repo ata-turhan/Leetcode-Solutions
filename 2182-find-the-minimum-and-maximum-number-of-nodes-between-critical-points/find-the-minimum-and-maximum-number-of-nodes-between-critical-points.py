38198259bd2007f6b9dac42b997a59754e4d618d
Ata Turhan <ataturhan21@gmail.com>
2024-07-05 03:11:40 +0300

    Time: 337 ms (83.27%) | Memory: 44.5 MB (14.45%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -3,35 +3,40 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
         critical_points = []
-
+        
+        # Initial values for previous and current nodes
         prev_prev_val = None
         prev_val = None
-        cur = head
-        i = -1
-        while cur:
-            if i < 1:
-                pass
-            elif prev_prev_val < prev_val and prev_val > cur.val:
-                critical_points.append(i)
-            elif prev_prev_val > prev_val and prev_val < cur.val:
-                critical_points.append(i)
-    
-            prev_prev_val = prev_val 
-            prev_val = cur.val
-            cur = cur.next
-            i += 1
+        current_node = head
+        index = -1
 
+        # Traverse the linked list to find critical points
+        while current_node:
+            if index >= 1:
+                if prev_prev_val < prev_val and prev_val > current_node.val:
+                    critical_points.append(index)
+                elif prev_prev_val > prev_val and prev_val < current_node.val:
+                    critical_points.append(index)
+
+            # Update previous values and move to the next node
+            prev_prev_val = prev_val
+            prev_val = current_node.val
+            current_node = current_node.next
+            index += 1
+
+        # If less than two critical points, return [-1, -1]
         if len(critical_points) < 2:
             return [-1, -1]
-        else:
-            min_diff = float("inf")
-            for i in range(1, len(critical_points)):
-                if critical_points[i] - critical_points[i-1] < min_diff:
-                    min_diff =    critical_points[i] - critical_points[i-1]
 
-            max_diff = critical_points[-1] - critical_points[0]
+        # Calculate minimum and maximum distances between critical points
+        min_diff = float("inf")
+        for i in range(1, len(critical_points)):
+            min_diff = min(min_diff, critical_points[i] - critical_points[i - 1])
 
-            return [min_diff, max_diff] 
+        max_diff = critical_points[-1] - critical_points[0]
+
+        return [min_diff, max_diff]
