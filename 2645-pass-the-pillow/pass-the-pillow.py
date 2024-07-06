24a230ecb5a193f3a623a9d5e01db4990b95341f
Ata Turhan <ataturhan21@gmail.com>
2024-07-06 03:40:22 +0300

    Time: 25 ms (96.96%) | Memory: 16.6 MB (30.38%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.py
+++ 2645-pass-the-pillow/pass-the-pillow.py
@@ -1,8 +1,8 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
-        players_in_a_round = n - 1  # Number of players involved in a full round
-        complete_rounds = time // players_in_a_round  # Number of complete rounds
-        remaining_time = time % players_in_a_round  # Remaining time after complete rounds
+        changes_in_a_round = n - 1  # Number of changes involved in a full round
+        complete_rounds = time // changes_in_a_round  # Number of complete rounds
+        remaining_time = time % changes_in_a_round  # Remaining time after complete rounds
 
         # Determine the position based on whether the number of complete rounds is even or odd
         if complete_rounds % 2 == 0:
