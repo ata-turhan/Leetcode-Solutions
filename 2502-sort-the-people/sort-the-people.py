5f7c6f5f3dba71871c7474b81a3cd7af34d4b664
Ata Turhan <ataturhan21@gmail.com>
2024-07-22 03:41:37 +0300

    Time: 108 ms (45.34%) | Memory: 17.2 MB (14.55%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,5 +1,8 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        # Combine heights and names into a list of tuples
         persons = list(zip(heights, names))
+        # Sort the list of tuples in descending order based on heights
         persons.sort(reverse=True)
+        # Extract the names from the sorted list of tuples
         return [name for height, name in persons]
