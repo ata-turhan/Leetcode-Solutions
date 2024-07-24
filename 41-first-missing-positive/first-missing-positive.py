1894d54f81b5f214dd71222ecfbe061cdacece92
Ata Turhan <ataturhan21@gmail.com>
2024-07-24 05:04:20 +0300

    Time: 392 ms (30.97%) | Memory: 30.2 MB (85.03%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,18 +1,13 @@
 from typing import List
 
 class Solution:
-    def swap(self, arr: List[int], a: int, b: int) -> List[int]:
-        # Swaps elements at indices a and b in the given array arr
-        arr[a], arr[b] = arr[b], arr[a]
-        return arr
-
     def firstMissingPositive(self, nums: List[int]) -> int:
         n = len(nums)
         for i in range(n):
             # Ensure nums[i] is within the range [1, n] and not already in its correct position
             while 1 <= nums[i] <= n and nums[i] != nums[nums[i] - 1]:
                 # Swap nums[i] with the element at index nums[i] - 1
-                nums = self.swap(nums, i, nums[i] - 1)
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
         
         # Iterate through nums to find the smallest missing positive integer
         for j in range(n):
