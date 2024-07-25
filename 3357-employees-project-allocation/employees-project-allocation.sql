f7d4b956c9e528a35365a585b10662547fd005fd
Ata Turhan <ataturhan21@gmail.com>
2024-07-25 05:27:24 +0300

    Time: 966 ms (27.78%) | Memory: 0B (100.00%) - LeetSync

--- 3357-employees-project-allocation/employees-project-allocation.sql
+++ 3357-employees-project-allocation/employees-project-allocation.sql
@@ -1,26 +1,26 @@
--- CTE to calculate the average workload per team
-WITH avg_workloads AS (
+-- CTE to calculate the average workload per team for each project
+WITH TeamAvgWorkloads AS (
     SELECT 
         e.employee_id,
         p.project_id,
         e.name AS employee_name,
         p.workload,
-        AVG(p.workload) OVER (PARTITION BY e.team) AS avg_workload
+        AVG(p.workload) OVER (PARTITION BY e.team) AS team_avg_workload
     FROM 
         employees AS e
     JOIN 
         project AS p USING (employee_id)
 )
 
--- Main query to select employees with workloads greater than the team average
+-- Main query to select employees with workloads greater than the team's average workload
 SELECT 
     employee_id, 
     project_id, 
     employee_name, 
     workload AS project_workload
 FROM 
-    avg_workloads 
+    TeamAvgWorkloads 
 WHERE 
-    workload > avg_workload 
+    workload > team_avg_workload 
 ORDER BY 
     employee_id, project_id;
