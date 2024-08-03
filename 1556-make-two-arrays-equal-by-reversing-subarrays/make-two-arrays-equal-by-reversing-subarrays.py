69e8af67698c3119b0b85ca79f80af6c5bc3a19b
Ata Turhan <ataturhan21@gmail.com>
2024-08-03 19:17:30 +0300

    Time: 73 ms (64.47%) | Memory: 16.8 MB (22.18%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
@@ -1,4 +1,7 @@
+from typing import List
+from collections import Counter
+
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        return sorted(arr) == sorted(target)
-        
+        # Count the frequency of each element in target and arr
+        return Counter(target) == Counter(arr)
