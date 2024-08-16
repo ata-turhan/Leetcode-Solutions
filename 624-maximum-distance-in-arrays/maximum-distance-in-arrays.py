3b32922e8bd8b010e383cd5d194b7d13c48cbd69
Ata Turhan <ataturhan21@gmail.com>
2024-08-16 04:06:45 +0300

    Time: 1387 ms (24.42%) | Memory: 42.4 MB (63.79%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
@@ -1,51 +1,17 @@
+from typing import List
+
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        first_min = 10**4+1
-        max_val = -10**4-1
-        idx_first_min = -1
-        first_max = -10**4-1
+        min_val = arrays[0][0]
+        max_val = arrays[0][-1]
+        max_distance = 0
 
-        for i, array in enumerate(arrays):
-            if array[0] < first_min:
-                first_min = array[0]
-                max_val = array[-1]
-                idx_first_min = i
-            elif array[0] == first_min and array[-1] < max_val:
-                max_val = array[-1]
-                idx_first_min = i
+        for i in range(1, len(arrays)):
+            # Compare with the current min and max values to find the possible max distance
+            max_distance = max(max_distance, abs(arrays[i][-1] - min_val), abs(max_val - arrays[i][0]))
 
-        for i, array in enumerate(arrays):
-            if i == idx_first_min:
-                continue
-            else:
-                if array[-1] > first_max:
-                    first_max = array[-1]
+            # Update min and max values
+            min_val = min(min_val, arrays[i][0])
+            max_val = max(max_val, arrays[i][-1])
 
-        second_min = 10**4+1
-        min_val = 10**4+1
-        idx_second_max = -1
-        second_max = -10**4-1
-
-        for i, array in enumerate(arrays):
-            if array[-1] > second_max:
-                second_max = array[-1]
-                min_val = array[0]
-                idx_second_max = i
-            elif array[-1] == second_max and array[0] > min_val:
-                min_val = array[0]
-                idx_second_max = i
-
-        for i, array in enumerate(arrays):
-            if i == idx_second_max:
-                continue
-            else:
-                if array[0] < second_min:
-                    second_min = array[0]
-
-        return max(first_max - first_min, second_max - second_min)
-
-
-
-                
-
-        
+        return max_distance
