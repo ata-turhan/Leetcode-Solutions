3bce1eeca51b7d81078352b8cf7ec0ae917470ba
Ata Turhan <ataturhan21@gmail.com>
2024-08-20 04:52:20 +0300

    Time: 122 ms (35.40%) | Memory: 16.8 MB (17.33%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.py
+++ 264-ugly-number-ii/ugly-number-ii.py
@@ -1,20 +1,19 @@
+import heapq
+
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        heap = [1]
-        ugly_nums = set()
-        for _ in range(n):
-            ugly_num = heapq.heappop(heap)
-            if ugly_num * 2 not in ugly_nums:
-                heapq.heappush(heap, ugly_num * 2)
-                ugly_nums.add(ugly_num * 2)
-            if ugly_num * 3 not in ugly_nums:
-                heapq.heappush(heap, ugly_num * 3)
-                ugly_nums.add(ugly_num * 3)
-            if ugly_num * 5 not in ugly_nums:
-                heapq.heappush(heap, ugly_num * 5)
-                ugly_nums.add(ugly_num * 5)
+        heap = [1]  # Min-heap to store the next potential ugly numbers
+        seen = {1}  # Set to keep track of numbers that have been added to the heap
 
-            print(ugly_num)
-            
-        return ugly_num
-        
+        for _ in range(n):
+            # Extract the smallest ugly number from the heap
+            ugly_num = heapq.heappop(heap)
+
+            # Generate the next potential ugly numbers by multiplying with 2, 3, and 5
+            for factor in [2, 3, 5]:
+                new_ugly = ugly_num * factor
+                if new_ugly not in seen:
+                    heapq.heappush(heap, new_ugly)
+                    seen.add(new_ugly)
+
+        return ugly_num  # The nth ugly number
