7d5bf51abd6f6a6dc8f26d71a1fb7c6ee29b18e1
Ata Turhan <ataturhan21@gmail.com>
2024-08-22 05:21:58 +0300

    Time: 37 ms (40.25%) | Memory: 16.5 MB (58.08%) - LeetSync

--- 476-number-complement/number-complement.py
+++ 476-number-complement/number-complement.py
@@ -1,9 +1,13 @@
 class Solution:
     def findComplement(self, num: int) -> int:
+        # Initialize a variable to track the bitmask of all 1s for the given number
         all_1s = 0
-        orig_num = num
-        while num > 0:
-            num >>= 1
-            all_1s <<= 1
-            all_1s |= 1
-        return all_1s - orig_num
+        temp = num
+
+        # Create the bitmask with all bits set to 1 corresponding to the number of bits in num
+        while temp > 0:
+            temp >>= 1  # Right shift temp to reduce its bits
+            all_1s = (all_1s << 1) | 1  # Left shift all_1s and add a 1 to the least significant bit
+
+        # The complement is found by subtracting the original number from the bitmask of all 1s
+        return all_1s ^ num  # XOR operation to get the complement
