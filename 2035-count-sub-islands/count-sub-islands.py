e3cf0d5313ac6f3949d473081bb3d82505e3db14
Ata Turhan <ataturhan21@gmail.com>
2024-08-28 04:22:51 +0300

    Time: 3006 ms (5.06%) | Memory: 97.5 MB (5.06%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.py
+++ 2035-count-sub-islands/count-sub-islands.py
@@ -1,45 +1,53 @@
+from typing import List
+
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        # Define possible directions for DFS exploration: right, left, up, down
         directions = [
-            [0, 1],
-            [0, -1],
-            [-1, 0],
-            [1, 0],
+            (0, 1),  # right
+            (0, -1), # left
+            (-1, 0), # up
+            (1, 0),  # down
         ]
         
-        def dfs(grid, rows, cols, i, j, visited):
-            visited.add((i, j))
-            for di, dj in directions:
-                new_i, new_j = i + di, j + dj
-                if new_i in range(rows1) and new_j in range(cols1) and grid[new_i][new_j] == 1 and (new_i, new_j) not in visited:
-                    dfs(grid, rows, cols, new_i, new_j, visited)
+        def dfs(grid, row, col, visited):
+            """Perform DFS to mark all cells of an island starting from (row, col)."""
+            visited.add((row, col))
+            for dr, dc in directions:
+                new_row, new_col = row + dr, col + dc
+                # Check if the new position is within grid boundaries and is part of the island
+                if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]) and grid[new_row][new_col] == 1 and (new_row, new_col) not in visited:
+                    dfs(grid, new_row, new_col, visited)
 
-        rows1, cols1 = len(grid1), len(grid1[0])
-        grid1_sub_islands = []
-        all_visited1 = set()
-        for i in range(rows1):
-            for j in range(cols1):
-                if grid1[i][j] == 1 and (i, j) not in all_visited1:
-                    visited = set()
-                    dfs(grid1, rows1, cols1, i, j, visited)
-                    grid1_sub_islands.append(visited)
-                    all_visited1 |= visited
+        num_rows, num_cols = len(grid1), len(grid1[0])
+        
+        # Find all islands in grid1
+        grid1_islands = []
+        visited_grid1 = set()
+        for row in range(num_rows):
+            for col in range(num_cols):
+                if grid1[row][col] == 1 and (row, col) not in visited_grid1:
+                    current_island = set()
+                    dfs(grid1, row, col, current_island)
+                    grid1_islands.append(current_island)
+                    visited_grid1 |= current_island
 
-        rows2, cols2 = len(grid2), len(grid2[0])
-        grid2_sub_islands = []
-        all_visited = set()
-        for i in range(rows2):
-            for j in range(cols2):
-                if grid2[i][j] == 1 and (i, j) not in all_visited:
-                    visited = set()
-                    dfs(grid2, rows2, cols2, i, j, visited)
-                    grid2_sub_islands.append(visited)
-                    all_visited |= visited
+        # Find all islands in grid2
+        grid2_islands = []
+        visited_grid2 = set()
+        for row in range(num_rows):
+            for col in range(num_cols):
+                if grid2[row][col] == 1 and (row, col) not in visited_grid2:
+                    current_island = set()
+                    dfs(grid2, row, col, current_island)
+                    grid2_islands.append(current_island)
+                    visited_grid2 |= current_island
 
+        # Count sub-islands in grid2 that are fully contained in grid1
         sub_island_count = 0
-        for island2 in grid2_sub_islands:
-            if len(island2 & all_visited1) >= len(island2):
+        for island2 in grid2_islands:
+            # If all cells of the island in grid2 are present in any island in grid1, count it as a sub-island
+            if island2 <= visited_grid1:
                 sub_island_count += 1
 
         return sub_island_count
-            
