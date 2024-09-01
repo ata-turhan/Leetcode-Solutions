d0a7d84e5ec4202a764c93b83e55b03455a3e478
Ata Turhan <ataturhan21@gmail.com>
2024-09-01 18:27:33 +0300

    Time: 61 ms (50.00%) | Memory: 16.7 MB (50.00%) - LeetSync

---
+++ 3540-hash-divided-string/hash-divided-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def stringHash(self, s: str, k: int) -> str:
+        result = ""
+        n = len(s)
+        
+        # Iterate over the string in chunks of size k
+        for i in range(0, n, k):
+            substr = s[i:i+k]  # Extract the substring of length k
+            # Calculate the hash sum as the sum of positional values of the characters
+            hash_sum = sum(ord(ch) - ord('a') for ch in substr)
+            # Determine the corresponding character for the hash sum modulo 26
+            hash_char = chr((hash_sum % 26) + ord('a'))
+            # Append the calculated character to the result string
+            result += hash_char
+        
+        return result
