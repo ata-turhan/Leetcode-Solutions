7a633fc626d2a72885e7f153b6b7f40b344735ba
Ata Turhan <ataturhan21@gmail.com>
2024-09-01 04:22:13 +0300

    Time: 665 ms (85.59%) | Memory: 23.6 MB (95.10%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -1,10 +1,16 @@
+from typing import List
+
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        # Check if the total number of elements matches the required 2D array dimensions
         if len(original) != m * n:
             return []
 
-        res = []
+        # Initialize the result 2D array
+        result = []
+        
+        # Construct the 2D array by slicing the original array into rows of length 'n'
         for i in range(0, len(original), n):
-            res.append(original[i:i+n])
+            result.append(original[i:i+n])
 
-        return res
+        return result
