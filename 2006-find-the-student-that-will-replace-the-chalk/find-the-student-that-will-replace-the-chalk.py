a3016de7b23ee921ccdbd64ef98c1c3c134ee655
Ata Turhan <ataturhan21@gmail.com>
2024-09-02 03:46:41 +0300

    Time: 591 ms (20.98%) | Memory: 29.9 MB (68.60%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
@@ -1,9 +1,12 @@
+from typing import List
+
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
-        total_chalk = sum(chalk)
-        k %= total_chalk
-        used = 0
-        for i, c in enumerate(chalk):
-            used += c
-            if k < used:
-                return i
+        total_chalk = sum(chalk)  # Calculate the total amount of chalk needed for one full cycle
+        k %= total_chalk  # Reduce k to the remainder when divided by total_chalk
+        
+        accumulated_chalk = 0  # Tracks the cumulative chalk used
+        for index, chalk_needed in enumerate(chalk):
+            accumulated_chalk += chalk_needed
+            if k < accumulated_chalk:
+                return index  # Return the index of the student who will use the last piece of chalk
