a159e35f2bf1777188908362bb7c93ee3cf8780c
Ata Turhan <ataturhan21@gmail.com>
2024-09-03 03:43:10 +0300

    Time: 245 ms (88.71%) | Memory: 0B (100.00%) - LeetSync

--- 3585-find-overlapping-shifts-ii/find-overlapping-shifts-ii.sql
+++ 3585-find-overlapping-shifts-ii/find-overlapping-shifts-ii.sql
@@ -49,8 +49,7 @@ OverlapDurations AS (
     JOIN EmployeeShifts e2 
         ON e1.employee_id = e2.employee_id 
         AND e1.start_time < e2.start_time
-    WHERE 
-        e1.end_time > e2.start_time
+        AND e1.end_time > e2.start_time
     GROUP BY 
         e1.employee_id
 )
