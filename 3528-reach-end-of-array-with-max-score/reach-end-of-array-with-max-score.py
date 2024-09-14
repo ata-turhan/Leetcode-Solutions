196302927573b8d25633a3d9a7ab8279283c1e07
Ata Turhan <ataturhan21@gmail.com>
2024-09-14 16:20:04 +0300

    Time: 1144 ms (67.39%) | Memory: 31.4 MB (46.36%) - LeetSync

---
+++ 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def findMaximumScore(self, nums: List[int]) -> int:
+        max_score: int = 0
+        index_stack: List[int] = [0]  # Stack to hold indexes, starting with the first element
+
+        # Iterate through nums to build the stack with increasing elements
+        for i in range(1, len(nums) - 1):
+            if nums[i] >= nums[index_stack[-1]]:
+                index_stack.append(i)
+
+        # Append the last index to the stack
+        index_stack.append(len(nums) - 1)
+
+        # Calculate the score by multiplying each valid range with its corresponding element
+        for i in range(1, len(index_stack)):
+            max_score += nums[index_stack[i - 1]] * (index_stack[i] - index_stack[i - 1])
+
+        return max_score
