6ee524abb149a0912ad009280a154e82c77abaed
Ata Turhan <ataturhan21@gmail.com>
2024-09-15 17:57:55 +0300

    Time: 508 ms (88.42%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -0,0 +1,10 @@
+SELECT 
+    employee_id,
+    CASE 
+        WHEN employee_id % 2 = 1 AND name NOT LIKE 'M%' THEN salary
+        ELSE 0
+    END AS bonus
+FROM 
+    employees
+ORDER BY 
+    employee_id;
