3eb7134499cc91437e0b855418aac6201eff934c
Ata Turhan <ataturhan21@gmail.com>
2024-09-18 03:44:46 +0300

    Time: 38 ms (74.95%) | Memory: 16.6 MB (32.99%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,27 +1,22 @@
+from functools import cmp_to_key
+from typing import List
+
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        lst = []
+        def compare(a: str, b: str) -> int:
+            # Compare by concatenating in both possible orders
+            if a + b > b + a:
+                return -1  # `a` should come before `b`
+            elif a + b < b + a:
+                return 1   # `b` should come before `a`
+            else:
+                return 0   # `a` and `b` are equal in comparison
 
-        for ele in nums:
-            lst += [str(ele)]
-        
-        n = len(lst)
+        # Convert all integers to strings
+        str_nums = [str(num) for num in nums]
+        # Sort using the custom comparator
+        str_nums.sort(key=cmp_to_key(compare))
 
-        for i in range(n):
-            for j in range(i+1 , n):
-                
-                if str(lst[i]) + str(lst[j]) > str(lst[j]) + str(lst[i]):
-                    # if current order is greatest value .. continue
-                    continue
-                else:
-                    # else swap the values ..!!!
-                    lst[i] , lst[j] = lst[j] , lst[i]
-        
-        
-        ans = ''.join(lst)
-
-        if int(ans) == 0:
-            return "0"
-        
-        return ans
-        
+        # Join sorted numbers and handle the edge case where the largest number is "0"
+        largest_number = "".join(str_nums)
+        return "0" if largest_number[0] == "0" else largest_number
