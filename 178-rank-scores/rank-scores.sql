e521eef1bc2d4fba638b52f5205e83760b8c9d42
Ata Turhan <ataturhan21@gmail.com>
2024-09-19 05:25:24 +0300

    Time: 345 ms (27.60%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,3 +1,11 @@
-select score, dense_rank() over (order by score desc) as "rank" 
-from scores
- order by score desc
+-- Select the score and calculate the dense rank for each score
+-- using the DENSE_RANK() function to ensure that identical scores get the same rank
+-- and no gaps in ranking for identical scores
+SELECT 
+    score, 
+    DENSE_RANK() OVER (ORDER BY score DESC) AS "rank"
+FROM 
+    scores
+-- Order the result by score in descending order to match the ranking
+ORDER BY 
+    score DESC;
