54f7feead8dbd3e75276f9182fb556a7cff1ff08
Ata Turhan <ataturhan21@gmail.com>
2024-09-30 04:07:40 +0300

    Time: 67 ms (94.21%) | Memory: 17.7 MB (18.01%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.py
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.py
@@ -3,6 +3,8 @@ class CustomStack:
     def __init__(self, maxSize: int):
         self.stack = []
         self.maxSize = maxSize
+        # To store incremental values lazily
+        self.increment_arr = [0] * maxSize
         
 
     def push(self, x: int) -> None:
@@ -11,16 +13,23 @@ class CustomStack:
         
 
     def pop(self) -> int:
-        if self.stack:
-            return self.stack.pop()
-        else:
+        if not self.stack:
             return -1
+        index = len(self.stack) - 1
+        # Apply any lazy increments
+        if index > 0:
+            self.increment_arr[index - 1] += self.increment_arr[index]
+        res = self.stack.pop() + self.increment_arr[index]
+        self.increment_arr[index] = 0  # Reset the increment for this index
+        return res
         
 
     def increment(self, k: int, val: int) -> None:
-        for i in range(min(k, len(self.stack))):
-            self.stack[i] += val
-        
+        # Apply the increment lazily for the first k elements
+        i = min(k, len(self.stack)) - 1
+        if i >= 0:
+            self.increment_arr[i] += val
+
 
 
 # Your CustomStack object will be instantiated and called as such:
