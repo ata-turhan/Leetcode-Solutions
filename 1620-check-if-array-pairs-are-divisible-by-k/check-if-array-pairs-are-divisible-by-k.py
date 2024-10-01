19e35f64779448972b7fc9fdc45e41be82523bfd
Ata Turhan <ataturhan21@gmail.com>
2024-10-01 05:02:18 +0300

    Time: 487 ms (74.89%) | Memory: 30.5 MB (27.88%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -1,18 +1,20 @@
+from collections import Counter
+from typing import List
+
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
-        arr = [num % k for num in arr]
-        zero_count = arr.count(0)
-        if zero_count % 2 == 1:
-            return False
-        arr_no_zeros = [num for num in arr if num != 0]
-        arr_no_zeros.sort()
-        l = 0
-        r = len(arr_no_zeros) - 1
-        while l < r:
-            if (arr_no_zeros[l] + arr_no_zeros[r]) == k:
-                l += 1
-                r -= 1
-            else:
-                return False
-        return True
+        # Count the remainders when dividing the numbers by k
+        remainder_count = Counter(num % k for num in arr)
         
+        for rem in remainder_count:
+            if rem == 0:  # Remainder 0 must have an even count
+                if remainder_count[rem] % 2 != 0:
+                    return False
+            elif 2 * rem == k:  # Special case where remainder is half of k (e.g., k=6, rem=3)
+                if remainder_count[rem] % 2 != 0:
+                    return False
+            else:  # General case: remainder and its complement must have the same count
+                if remainder_count[rem] != remainder_count[k - rem]:
+                    return False
+
+        return True
