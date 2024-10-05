6199e919e9da06bc502e58d9806a2c46cdaed71b
Ata Turhan <ataturhan21@gmail.com>
2024-10-05 04:47:46 +0300

    Time: 993 ms (87.37%) | Memory: 0B (100.00%) - LeetSync

--- 1269-market-analysis-ii/market-analysis-ii.sql
+++ 1269-market-analysis-ii/market-analysis-ii.sql
@@ -1,49 +1,49 @@
-WITH inactive_sellers AS (
-    -- Select users who have fewer than 2 orders and classify them as inactive sellers
+WITH InactiveSellers AS (
+    -- Select users with fewer than 2 orders and classify them as inactive sellers
     SELECT 
-        user_id, 
-        'no' AS "2nd_item_fav_brand" 
+        u.user_id, 
+        'no' AS "2nd_item_fav_brand"
     FROM 
-        orders 
-    RIGHT JOIN 
-        users ON orders.seller_id = users.user_id 
+        users u
+    LEFT JOIN 
+        orders o ON u.user_id = o.seller_id 
     GROUP BY 
-        user_id 
+        u.user_id 
     HAVING 
-        COUNT(order_id) < 2
+        COUNT(o.order_id) < 2
 ),
-active_sellers AS (
+ActiveSellers AS (
     -- Select orders of sellers who have at least 2 orders, excluding inactive sellers
     SELECT 
         * 
     FROM 
-        orders 
+        orders o
     WHERE 
-        seller_id NOT IN (SELECT user_id FROM inactive_sellers)
+        o.seller_id NOT IN (SELECT user_id FROM InactiveSellers)
 ),
-active_sellers_rank AS (
+RankedActiveSellers AS (
     -- Rank orders for active sellers to determine the second order (rank 2)
     SELECT 
-        *, 
-        ROW_NUMBER() OVER (PARTITION BY seller_id ORDER BY order_date ASC) AS ranks 
+        o.*, 
+        ROW_NUMBER() OVER (PARTITION BY o.seller_id ORDER BY o.order_date ASC) AS order_rank 
     FROM 
-        active_sellers
+        ActiveSellers o
 )
 -- Retrieve the second order of active sellers and check if it matches their favorite brand
 SELECT 
-    asr.seller_id, 
-    IF(u.favorite_brand = i.item_brand, 'yes', 'no') AS "2nd_item_fav_brand" 
+    ras.seller_id, 
+    IF(u.favorite_brand = i.item_brand, 'yes', 'no') AS "2nd_item_fav_brand"
 FROM 
-    active_sellers_rank AS asr 
+    RankedActiveSellers ras
 LEFT JOIN 
-    users AS u ON asr.seller_id = u.user_id 
+    users u ON ras.seller_id = u.user_id 
 LEFT JOIN 
-    items AS i ON asr.item_id = i.item_id 
+    items i ON ras.item_id = i.item_id 
 WHERE 
-    ranks = 2
--- Combine with the inactive sellers who have no second order
+    ras.order_rank = 2
+-- Combine with inactive sellers who do not have a second order
 UNION ALL
 SELECT 
-    * 
+    *
 FROM 
-    inactive_sellers;
+    InactiveSellers isel;
