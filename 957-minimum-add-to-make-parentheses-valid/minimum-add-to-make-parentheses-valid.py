da7597b2376a12a80370be57b0dc18692ec3d90d
Ata Turhan <ataturhan21@gmail.com>
2024-10-09 05:13:22 +0300

    Time: 36 ms (48.13%) | Memory: 16.7 MB (30.64%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -1,14 +1,20 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
+        # Initialize counters for unbalanced open and close parentheses
         unbalanced_open, unbalanced_close = 0, 0
+        
+        # Iterate through the string
         for char in s:
             if char == "(":
+                # Increment unbalanced_open for each open parenthesis
                 unbalanced_open += 1
             else:
+                # If there's an unbalanced open parenthesis, balance it
                 if unbalanced_open > 0:
                     unbalanced_open -= 1
                 else:
+                    # Otherwise, increment unbalanced_close for each extra close parenthesis
                     unbalanced_close += 1
 
+        # The result is the sum of unbalanced open and close parentheses
         return unbalanced_open + unbalanced_close
-        
