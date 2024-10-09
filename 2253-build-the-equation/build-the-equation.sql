0903c7d76fa6809630c25842e4bb55d228768011
Ata Turhan <ataturhan21@gmail.com>
2024-10-09 05:30:42 +0300

    Time: 547 ms (29.87%) | Memory: 0B (100.00%) - LeetSync

--- 2253-build-the-equation/build-the-equation.sql
+++ 2253-build-the-equation/build-the-equation.sql
@@ -1,14 +1,23 @@
-with terms_vertical as (
-    select power,
-case
-when power = 0 then
-if(substr(factor, 1, 1) = "-", factor, concat("+", factor))
-when power = 1 then
-if(substr(factor, 1, 1) = "-", concat(factor, "X"), concat("+", factor, "X"))
-else
-if(substr(factor, 1, 1) = "-", concat(factor, "X", "^", power), concat("+", factor, "X", "^", power))
-end
-as term
-from terms
+WITH FormattedTerms AS (
+    -- Format each term of the polynomial equation based on its power and factor
+    SELECT 
+        power,
+        CASE
+            WHEN power = 0 THEN
+                -- Constant term: add '+' if factor is positive
+                IF(SUBSTR(factor, 1, 1) = '-', factor, CONCAT('+', factor))
+            WHEN power = 1 THEN
+                -- Linear term: append 'X' to the factor
+                IF(SUBSTR(factor, 1, 1) = '-', CONCAT(factor, 'X'), CONCAT('+', factor, 'X'))
+            ELSE
+                -- Higher-degree terms: append 'X^power' to the factor
+                IF(SUBSTR(factor, 1, 1) = '-', CONCAT(factor, 'X^', power), CONCAT('+', factor, 'X^', power))
+        END AS term
+    FROM 
+        terms
 )
-select CONCAT(group_concat(term order by power desc  SEPARATOR ""), "=0") as equation from terms_vertical
+-- Concatenate all formatted terms in descending order of power to form the equation
+SELECT 
+    CONCAT(GROUP_CONCAT(term ORDER BY power DESC SEPARATOR ''), '=0') AS equation
+FROM 
+    FormattedTerms;
