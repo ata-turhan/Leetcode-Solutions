aeeb35393d0232e8234dbd899869f327d1eef4a4
Ata Turhan <ataturhan21@gmail.com>
2024-10-19 04:36:34 +0300

    Time: 415 ms (35.90%) | Memory: 24.1 MB (31.36%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
@@ -1,14 +1,19 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        def findNthNum(n):
+        # Function to generate the nth string
+        def generateNthString(n: int) -> str:
             if n == 1:
                 return "0"
-            s = [0]
-            for _ in range(1, n):
-                s = s + [1] + list(reversed([1-num for num in s]))
-            return "".join(map(str, s))
             
-        num = findNthNum(n)
-        print(num)
-        return num[k-1]
+            # Recursively generate the previous string
+            prev_string = generateNthString(n - 1)
+            
+            # Invert and reverse the previous string
+            inverted_reversed = ''.join('1' if char == '0' else '0' for char in reversed(prev_string))
+            
+            # Concatenate previous string + "1" + inverted and reversed string
+            return prev_string + "1" + inverted_reversed
         
+        # Generate the nth string and return the k-th bit
+        nth_string = generateNthString(n)
+        return nth_string[k - 1]
