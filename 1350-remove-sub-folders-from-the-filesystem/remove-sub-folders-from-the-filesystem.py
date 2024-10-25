bdac1b19dba8017aaa291bbf2aa1894f8ecabf5e
Ata Turhan <ataturhan21@gmail.com>
2024-10-25 03:24:12 +0300

    Time: 47 ms (87.76%) | Memory: 29.6 MB (93.10%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,22 +1,28 @@
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
+        # Initialize a list to store the main folders and a set to track seen folders
         main_folders = []
         seen_folders = set()
+
+        # Sort the folder list to ensure subfolders appear after their parent folders
         folder.sort()
 
+        # Iterate over each folder in the sorted list
         for f in folder:
-            sub_folders = f[1:].split("/")
-            full_name = ""
-            for f_name in sub_folders:
-                full_name += "/" + f_name
+            sub_folders = f[1:].split("/")  # Remove leading '/' and split into subfolders
+            full_name = ""  # To build the folder path incrementally
+            
+            # Check each subfolder to see if it's already in seen_folders
+            for sub_name in sub_folders:
+                full_name += "/" + sub_name
                 if full_name in seen_folders:
-                    break
-                    
+                    break  # If a parent folder is found, stop processing this folder
+            
+            # If the entire folder path is not a subfolder of any previously seen folder
             if full_name in seen_folders:
-                    continue
+                continue  # Skip if already part of seen folders
             else:
-                seen_folders.add(full_name)
-                main_folders.append(full_name)
+                seen_folders.add(full_name)  # Add the current folder to seen
+                main_folders.append(f)  # Add the folder to the main list
 
         return main_folders
-
