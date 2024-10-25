bc9b405678a3d21fb87388fd60724e09d29a3dca
Ata Turhan <ataturhan21@gmail.com>
2024-10-25 03:24:58 +0300

    Time: 46 ms (88.10%) | Memory: 29.7 MB (82.93%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,28 +1,27 @@
 class Solution:
-    def removeSubfolders(self, folder: List[str]) -> List[str]:
-        # Initialize a list to store the main folders and a set to track seen folders
+    def removeSubfolders(self, folder_paths: List[str]) -> List[str]:
         main_folders = []
-        seen_folders = set()
+        folder_tracker = set()
 
-        # Sort the folder list to ensure subfolders appear after their parent folders
-        folder.sort()
+        # Sort folder paths to ensure subfolders appear after their parent folders
+        folder_paths.sort()
 
         # Iterate over each folder in the sorted list
-        for f in folder:
-            sub_folders = f[1:].split("/")  # Remove leading '/' and split into subfolders
-            full_name = ""  # To build the folder path incrementally
+        for folder_path in folder_paths:
+            subfolder_parts = folder_path[1:].split("/")  # Remove leading '/' and split into subfolder parts
+            accumulated_path = ""  # To build the folder path incrementally
             
-            # Check each subfolder to see if it's already in seen_folders
-            for sub_name in sub_folders:
-                full_name += "/" + sub_name
-                if full_name in seen_folders:
+            # Check if the current folder is a subfolder of any previously tracked folder
+            for part in subfolder_parts:
+                accumulated_path += "/" + part
+                if accumulated_path in folder_tracker:
                     break  # If a parent folder is found, stop processing this folder
             
-            # If the entire folder path is not a subfolder of any previously seen folder
-            if full_name in seen_folders:
-                continue  # Skip if already part of seen folders
+            # If the entire folder path is not a subfolder of any previously tracked folder
+            if accumulated_path in folder_tracker:
+                continue  # Skip if already part of folder_tracker
             else:
-                seen_folders.add(full_name)  # Add the current folder to seen
-                main_folders.append(f)  # Add the folder to the main list
+                folder_tracker.add(accumulated_path)  # Add the current folder to the tracker
+                main_folders.append(folder_path)  # Add the folder to the main folder list
 
         return main_folders
