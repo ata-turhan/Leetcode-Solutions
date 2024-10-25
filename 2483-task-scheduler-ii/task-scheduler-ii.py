2c4aec440957419f7bad22d5d9234e78feb515d7
Ata Turhan <ataturhan21@gmail.com>
2024-10-25 05:57:56 +0300

    Time: 87 ms (97.89%) | Memory: 35.7 MB (9.14%) - LeetSync

--- 2483-task-scheduler-ii/task-scheduler-ii.py
+++ 2483-task-scheduler-ii/task-scheduler-ii.py
@@ -1,11 +1,16 @@
+from collections import defaultdict
+
 class Solution:
-    def taskSchedulerII(self, tasks: List[int], space: int) -> int:
-        elapsed_time = 0
-        earliest_start = defaultdict(int)
+    def taskSchedulerII(self, tasks: List[int], cooldown: int) -> int:
+        current_day = 0
+        next_available_day = defaultdict(int)
+
         for task in tasks:
-            elapsed_time += 1
-            if earliest_start[task] > elapsed_time:
-                elapsed_time = earliest_start[task] 
-            earliest_start[task] = elapsed_time + space + 1
-        return elapsed_time
-                       
+            current_day += 1
+            # If the task can't be scheduled yet, wait until the next available day
+            if next_available_day[task] > current_day:
+                current_day = next_available_day[task]
+            # Schedule the task and update when it can next be executed
+            next_available_day[task] = current_day + cooldown + 1
+
+        return current_day
