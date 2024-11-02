7650bc69a12c663ff9ab2b1e9e8c1c351ca6e3d0
Ata Turhan <ataturhan21@gmail.com>
2024-11-02 05:12:39 +0300

    Time: 303 ms (64.46%) | Memory: 18.7 MB (34.25%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -1,15 +1,18 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        stack = []
+        result = []
 
+        # Iterate through each character in the string
         for char in s:
-            if len(stack) < 2:
-                stack.append(char)
+            # If result has fewer than two characters, simply add the current character
+            if len(result) < 2:
+                result.append(char)
             else:
-                if char == stack[-1] == stack[-2]:
-                    pass
+                # Check if the last two characters are the same as the current character
+                if char == result[-1] == result[-2]:
+                    continue  # Skip appending the character to avoid three consecutive duplicates
                 else:
-                    stack.append(char)
+                    result.append(char)
         
-        return "".join(stack)
-        
+        # Join and return the list as a final fancy string
+        return "".join(result)
