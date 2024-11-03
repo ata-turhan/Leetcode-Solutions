74fe5705e2895b799ae56c307fa3ada94df2cf10
Ata Turhan <ataturhan21@gmail.com>
2024-11-03 04:34:41 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (40.35%) - LeetSync

--- 812-rotate-string/rotate-string.py
+++ 812-rotate-string/rotate-string.py
@@ -1,3 +1,5 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
+        # Check if 'goal' can be obtained by rotating 's'
+        # This is true if 'goal' is a substring of 's + s' and both strings have the same length
         return len(goal) == len(s) and goal in (s + s)
