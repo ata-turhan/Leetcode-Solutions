67f851493ee603e1fc53e0babff2fdb8e3958ca9
Ata Turhan <ataturhan21@gmail.com>
2024-11-05 04:29:44 +0300

    Time: 52 ms (6.76%) | Memory: 18.2 MB (6.10%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -1,26 +1,29 @@
+from typing import List
+
 class Solution:
     def minChanges(self, s: str) -> int:
-        cons_counts = []
-        prev_char = ""
-        char_count = 0
+        consecutive_counts = []  # Stores counts of consecutive identical characters
+        previous_char = ""
+        current_count = 0
         
+        # Count consecutive characters
         for char in s:
-            if char == prev_char:
-                char_count += 1
+            if char == previous_char:
+                current_count += 1
             else:
-                if char_count != 0:
-                    cons_counts.append(char_count)
-                prev_char = char
-                char_count = 1
-        cons_counts.append(char_count)
+                if current_count != 0:
+                    consecutive_counts.append(current_count)
+                previous_char = char
+                current_count = 1
+        consecutive_counts.append(current_count)  # Append last count
 
-        change_count = 0
-        for i in range(len(cons_counts)):
-            if cons_counts[i] % 2 != 0:
-                change_count += 1
-                if i < len(cons_counts) - 1:
-                    cons_counts[i+1] += 1
+        # Calculate minimum changes needed
+        changes_needed = 0
+        for i in range(len(consecutive_counts)):
+            # If count is odd, increment changes and adjust the next count
+            if consecutive_counts[i] % 2 != 0:
+                changes_needed += 1
+                if i < len(consecutive_counts) - 1:
+                    consecutive_counts[i + 1] += 1
 
-        return change_count
-                
-        
+        return changes_needed
