eee05de9e2fa1e0a8f8fdfb1e92e3cbf929eadb9
Ata Turhan <ataturhan21@gmail.com>
2024-11-06 04:51:32 +0300

    Time: 7 ms (78.18%) | Memory: 16.8 MB (23.35%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
@@ -2,31 +2,32 @@ from typing import List
 
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-        # Function to get the count of '1' bits in the binary representation of a number
-        def count_bits(num):
+        # Helper function to count '1' bits in the binary representation of a number
+        def count_ones_in_binary(num):
             return bin(num).count("1")
 
-        # Cache bit counts for each unique number in nums
-        bit_counts = {num: count_bits(num) for num in set(nums)}
+        # Dictionary to store the bit count for each unique number in nums
+        bit_count_map = {num: count_ones_in_binary(num) for num in set(nums)}
 
-        # Create groups of numbers based on their bit counts, tracking min and max in each group
-        groups = []
-        current_bit_count = bit_counts[nums[0]]
-        min_num, max_num = nums[0], nums[0]
+        # List to track min and max values in each contiguous group with the same bit count
+        min_max_groups = []
+        current_bit_count = bit_count_map[nums[0]]
+        current_min, current_max = nums[0], nums[0]
 
+        # Iterate through nums to form groups based on bit counts
         for num in nums:
-            # If current num has the same bit count as previous, update min and max in the current group
-            if bit_counts[num] == current_bit_count:
-                min_num = min(min_num, num)
-                max_num = max(max_num, num)
+            # Update min and max for the current group if bit count is the same
+            if bit_count_map[num] == current_bit_count:
+                current_min = min(current_min, num)
+                current_max = max(current_max, num)
             else:
-                # Append min and max of the finished group and reset for new group
-                groups.extend([min_num, max_num])
-                current_bit_count = bit_counts[num]
-                min_num, max_num = num, num
+                # Append min and max of completed group and start a new group
+                min_max_groups.extend([current_min, current_max])
+                current_bit_count = bit_count_map[num]
+                current_min, current_max = num, num
 
         # Append the min and max of the last group
-        groups.extend([min_num, max_num])
+        min_max_groups.extend([current_min, current_max])
 
-        # Check if the groups' min-max values are sorted, indicating array can be sorted with swaps
-        return groups == sorted(groups)
+        # Check if the min-max pairs are sorted to confirm if array can be sorted by group swaps
+        return min_max_groups == sorted(min_max_groups)
