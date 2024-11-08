c4a360ec873f2ecae34bd3f0ac6794a1b09614e9
Ata Turhan <ataturhan21@gmail.com>
2024-11-08 06:51:54 +0300

    Time: 798 ms (5.36%) | Memory: 32.3 MB (84.59%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -1,15 +1,24 @@
+from typing import List
+from functools import reduce
+
 class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        # Calculate the XOR of all numbers in nums
         total_xor = reduce(lambda a, b: a ^ b, nums)
-        queries = []
+        results = []
 
-        for i in range(len(nums)-1, -1, -1):
-            k = 0
-            for j in range(maximumBit):
-                k |= (((total_xor >> j) & 1) ^ 1) << j
-            queries.append(k)
+        # Iterate from the last index to the first
+        for i in range(len(nums) - 1, -1, -1):
+            # Initialize k to hold the maximum XOR result for the current total_xor
+            max_xor = 0
+            for bit_position in range(maximumBit):
+                # Set each bit in k to the opposite of the corresponding bit in total_xor
+                max_xor |= (((total_xor >> bit_position) & 1) ^ 1) << bit_position
 
+            # Append the current max_xor result to the results list
+            results.append(max_xor)
+
+            # Update total_xor by removing the effect of nums[i]
             total_xor ^= nums[i]
 
-        return queries
-        
+        return results
