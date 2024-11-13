1edf85885e7247e84d485f0efe47cbd61e53fdb6
Ata Turhan <ataturhan21@gmail.com>
2024-11-13 04:39:09 +0300

    Time: 222 ms (54.76%) | Memory: 30.5 MB (91.73%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
@@ -1,22 +1,16 @@
+from bisect import bisect_left, bisect_right
+from typing import List
+
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
         num_pairs = 0
-        print(nums)
+        n = len(nums)
 
-        for i, num in enumerate(nums):
-            smallest_pair_idx = bisect.bisect_left(nums, lower - num)
-            largest_pair_idx  = bisect.bisect_right(nums, upper - num)
-            if smallest_pair_idx == len(nums):
-                continue
-            if largest_pair_idx == 0:
-                continue
-            largest_pair_idx -= 1
-            smallest_pair_idx = max(smallest_pair_idx, i + 1)
-            largest_pair_idx = max(largest_pair_idx, i + 1)
-            if largest_pair_idx >= smallest_pair_idx and smallest_pair_idx < len(nums) and largest_pair_idx < len(nums) and num + nums[smallest_pair_idx] >= lower and num + nums[largest_pair_idx] <= upper:
-                num_pairs += (largest_pair_idx - smallest_pair_idx + 1)
-                print(num, smallest_pair_idx, largest_pair_idx)
+        for i in range(n):
+            num = nums[i]
+            left = bisect_left(nums, lower - num, i + 1)
+            right = bisect_right(nums, upper - num, i + 1)
+            num_pairs += right - left
 
         return num_pairs
-        
