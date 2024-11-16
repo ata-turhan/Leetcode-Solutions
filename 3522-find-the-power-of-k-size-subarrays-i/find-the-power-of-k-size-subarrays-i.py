636600d4a638e0c39270cf72d1c502fdfc103105
Ata Turhan <ataturhan21@gmail.com>
2024-11-16 05:14:58 +0300

    Time: 7 ms (57.45%) | Memory: 16.8 MB (21.46%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
@@ -1,23 +1,33 @@
+from typing import List
+from collections import deque
+
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-        res = []
-        stack = deque([0])
-        for i in range(1, k-1):
-            while stack and nums[stack[-1]] != (nums[i] - 1):
-                stack.pop()
-            stack.append(i)
+        results = []  # Stores the resulting array
+        index_stack = deque([0])  # Deque to keep track of valid indices
 
-        for i in range(k-1, len(nums)):
-            while stack and nums[stack[-1]] != (nums[i] - 1):
-                stack.pop()
-            stack.append(i)
-            if len(stack) == k:
-                res.append(nums[stack[-1]])
+        # Initial processing for the first k-1 elements
+        for i in range(1, k - 1):
+            # Maintain stack only for indices where nums[stack[-1]] == nums[i] - 1
+            while index_stack and nums[index_stack[-1]] != (nums[i] - 1):
+                index_stack.pop()
+            index_stack.append(i)
+
+        # Process the remaining elements
+        for i in range(k - 1, len(nums)):
+            # Maintain stack only for indices where nums[stack[-1]] == nums[i] - 1
+            while index_stack and nums[index_stack[-1]] != (nums[i] - 1):
+                index_stack.pop()
+            index_stack.append(i)
+
+            # If the stack size matches k, append the result; otherwise, append -1
+            if len(index_stack) == k:
+                results.append(nums[index_stack[-1]])
             else:
-                res.append(-1)
-            if stack[0] == (i - k + 1): 
-                stack.popleft()
+                results.append(-1)
 
-        return res
+            # Remove indices that fall out of the current window
+            if index_stack[0] == (i - k + 1): 
+                index_stack.popleft()
 
-        
+        return results
