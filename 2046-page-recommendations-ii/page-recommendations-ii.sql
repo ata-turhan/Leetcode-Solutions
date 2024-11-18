c1c6bc7a16bfd32da6a1f2ebce28d98b7032ddd0
Ata Turhan <ataturhan21@gmail.com>
2024-11-18 03:55:32 +0300

    Time: 1076 ms (82.65%) | Memory: 0B (100.00%) - LeetSync

--- 2046-page-recommendations-ii/page-recommendations-ii.sql
+++ 2046-page-recommendations-ii/page-recommendations-ii.sql
@@ -1,11 +1,25 @@
-with all_friends as (
-    select * from friendship
-    union all
-    select user2_id, user1_id from friendship
+WITH all_friends AS (
+    -- Create a bidirectional friendship relationship
+    SELECT user1_id AS user_id, user2_id AS friend_id
+    FROM friendship
+    UNION ALL
+    SELECT user2_id AS user_id, user1_id AS friend_id
+    FROM friendship
 )
-select af.user1_id as user_id, l1.page_id, count(af.user2_id) as friends_likes  
-from all_friends as af 
-left join likes as l1 on af.user2_id = l1.user_id 
-left join likes as l2 on af.user1_id = l2.user_id and l1.page_id = l2.page_id
-where l2.page_id is null
-group by af.user1_id, l1.page_id
+SELECT 
+    af.user_id AS user_id,            -- The user ID
+    l1.page_id AS page_id,           -- The page ID liked by the user's friends
+    COUNT(af.friend_id) AS friends_likes -- Number of friends who liked the page
+FROM 
+    all_friends AS af
+LEFT JOIN 
+    likes AS l1 ON af.friend_id = l1.user_id -- Join friends with their liked pages
+LEFT JOIN 
+    likes AS l2 ON af.user_id = l2.user_id AND l1.page_id = l2.page_id 
+    -- Check if the user has liked the page their friends liked
+WHERE 
+    l2.page_id IS NULL -- Exclude pages liked by the user
+GROUP BY 
+    af.user_id, l1.page_id -- Group by user and page ID
+ORDER BY 
+    af.user_id, l1.page_id; -- Optional ordering for clarity
