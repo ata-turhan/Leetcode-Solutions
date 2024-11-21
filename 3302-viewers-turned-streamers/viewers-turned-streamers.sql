ca4b48397a90f73bf33a611fe1a072d7103f37e1
Ata Turhan <ataturhan21@gmail.com>
2024-11-21 06:12:43 +0300

    Time: 359 ms (46.62%) | Memory: 0B (100.00%) - LeetSync

--- 3302-viewers-turned-streamers/viewers-turned-streamers.sql
+++ 3302-viewers-turned-streamers/viewers-turned-streamers.sql
@@ -1,9 +1,31 @@
-with ranked_sessions as (
-    select *, rank() over (partition by user_id order by session_start asc) as ranks from sessions
+WITH RankedSessions AS (
+    SELECT 
+        *,
+        RANK() OVER (PARTITION BY user_id ORDER BY session_start ASC) AS session_rank
+    FROM sessions
 ),
-first_viewers as (
-    select user_id from ranked_sessions where ranks = 1 and session_type = "Viewer"
+FirstViewers AS (
+    SELECT 
+        user_id
+    FROM 
+        RankedSessions
+    WHERE 
+        session_rank = 1 
+        AND session_type = 'Viewer'
 )
-select user_id, 
-sum(if(session_type = "Streamer", 1, 0)) as sessions_count
- from sessions join first_viewers using(user_id) group by user_id having sessions_count > 0 order by 2 desc, 1 desc;
+SELECT 
+    s.user_id, 
+    SUM(CASE WHEN s.session_type = 'Streamer' THEN 1 ELSE 0 END) AS sessions_count 
+FROM 
+    sessions s
+JOIN 
+    FirstViewers fv
+USING 
+    (user_id)
+GROUP BY 
+    s.user_id
+HAVING 
+    sessions_count  > 0
+ORDER BY 
+    sessions_count  DESC, 
+    s.user_id DESC;
