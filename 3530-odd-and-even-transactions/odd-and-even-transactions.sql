fecd8ab6b292be25fac8d619f5e1700483dc9690
Ata Turhan <ataturhan21@gmail.com>
2024-11-25 06:47:16 +0300

    Time: 318 ms (57.81%) | Memory: 0B (100.00%) - LeetSync

--- 3530-odd-and-even-transactions/odd-and-even-transactions.sql
+++ 3530-odd-and-even-transactions/odd-and-even-transactions.sql
@@ -1,6 +1,10 @@
-select transaction_date, 
-sum(if(amount % 2 = 1, amount, 0)) as odd_sum, 
-sum(if(amount % 2 = 0, amount, 0)) as even_sum 
-from transactions 
-group by transaction_date 
-order by transaction_date asc;
+SELECT 
+    transaction_date, 
+    SUM(CASE WHEN amount % 2 = 1 THEN amount ELSE 0 END) AS odd_sum, 
+    SUM(CASE WHEN amount % 2 = 0 THEN amount ELSE 0 END) AS even_sum 
+FROM 
+    transactions 
+GROUP BY 
+    transaction_date 
+ORDER BY 
+    transaction_date ASC;
