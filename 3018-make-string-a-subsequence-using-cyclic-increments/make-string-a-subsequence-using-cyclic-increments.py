01e15bc455fb8784bc8488300d5d07bb9850b567
Ata Turhan <ataturhan21@gmail.com>
2024-12-04 03:34:31 +0300

    Time: 71 ms (74.31%) | Memory: 17.5 MB (50.24%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -1,11 +1,18 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        idx = 0
-        for char in str1:
-            if char == str2[idx] or (chr((ord(char) - ord("a") + 1) % 26 + ord("a")) == str2[idx]):
-                idx += 1
-                if idx == len(str2):
-                    break
-
-        return idx == len(str2)
+        subsequence_index = 0
         
+        # Iterate through each character in str1
+        for char in str1:
+            # Check if the current character matches the required character in str2
+            # or if it can be incremented cyclically to match
+            if (char == str2[subsequence_index] or 
+                chr((ord(char) - ord('a') + 1) % 26 + ord('a')) == str2[subsequence_index]):
+                subsequence_index += 1
+                
+                # If the entire str2 is matched, return True
+                if subsequence_index == len(str2):
+                    return True
+        
+        # Return whether all characters of str2 were matched
+        return subsequence_index == len(str2)
