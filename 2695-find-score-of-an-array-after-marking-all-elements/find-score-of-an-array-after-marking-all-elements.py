00586ccfe510e6509ba7ac32ae5bb7dbe15627f0
Ata Turhan <ataturhan21@gmail.com>
2024-12-13 06:58:17 +0300

    Time: 685 ms (20.73%) | Memory: 38.9 MB (56.27%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
@@ -1,16 +1,31 @@
+from heapq import heapify, heappop
+from typing import List
+
 class Solution:
     def findScore(self, nums: List[int]) -> int:
-        marked = set()
-        nums_indices = [(num, i) for i, num in enumerate(nums)]
-        heapify(nums_indices)
+        """
+        Finds the score by selecting the smallest unmarked number and marking its adjacent indices.
+
+        :param nums: List of integers representing the input numbers.
+        :return: Integer representing the calculated score.
+        """
+        # Set to keep track of marked indices
+        marked_indices = set()
+        
+        # Create a heap of (value, index) pairs for processing in increasing order
+        value_index_pairs = [(num, index) for index, num in enumerate(nums)]
+        heapify(value_index_pairs)
+
         score = 0
 
-        while nums_indices:
-            num, i = heappop(nums_indices)
-            if i not in marked:
-                score += num
-                marked.add(i-1)
-                marked.add(i+1)
+        # Process heap until empty
+        while value_index_pairs:
+            num, index = heappop(value_index_pairs)
+            
+            # If the current index is not already marked, process it
+            if index not in marked_indices:
+                score += num  # Add the value to the score
+                marked_indices.add(index - 1)  # Mark left neighbor
+                marked_indices.add(index + 1)  # Mark right neighbor
 
         return score
-        
