a929c3d818d597e089cf019e888db4830bc9ba5f
Ata Turhan <ataturhan21@gmail.com>
2025-01-07 07:03:31 +0300

    Time: 15 ms (8.18%) | Memory: 18.1 MB (6.61%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -1,17 +1,28 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        substrings = set()
-        for word in words:
-            for i in range(len(word)):
-                for j in range(i, len(word)):
-                    if i == 0 and j == len(word) - 1:
-                        continue
-                    substrings.add(word[i:j+1])
+        """
+        Finds all words in the list that are substrings of other words in the list.
 
-        res = []
-        for word in words:
-            if word in substrings:
-                res.append(word)
-
-        return res
+        :param words: List of strings.
+        :return: List of words that are substrings of other words.
+        """
+        # Set to store all substrings found across the words
+        substring_set = set()
         
+        # Collect all substrings from the words
+        for word in words:
+            for start_idx in range(len(word)):
+                for end_idx in range(start_idx, len(word)):
+                    # Skip adding the entire word itself to the substring set
+                    if start_idx == 0 and end_idx == len(word) - 1:
+                        continue
+                    # Add substring [start_idx:end_idx+1] to the set
+                    substring_set.add(word[start_idx:end_idx+1])
+
+        # List to store results for words that are found in the substring set
+        result = []
+        for word in words:
+            if word in substring_set:  # Check if the current word is a substring of any word
+                result.append(word)
+
+        return result
