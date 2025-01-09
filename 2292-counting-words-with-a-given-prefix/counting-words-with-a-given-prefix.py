c012f1f512359c34bcf0441b1646f9987b4517a1
Ata Turhan <ataturhan21@gmail.com>
2025-01-09 07:01:30 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (20.46%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
@@ -1,4 +1,11 @@
 class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
+        """
+        Count the number of words that start with a given prefix.
+
+        :param words: List[str] - List of words to search.
+        :param pref: str - The prefix to check.
+        :return: int - Count of words starting with the prefix.
+        """
+        # Use a generator expression to check if each word starts with the prefix and sum the results
         return sum(word.startswith(pref) for word in words)
-        
