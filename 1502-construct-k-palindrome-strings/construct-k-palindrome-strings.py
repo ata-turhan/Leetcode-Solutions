a07bad4bcf5f62325535b856abfb7390dddaf3c1
Ata Turhan <ataturhan21@gmail.com>
2025-01-11 06:41:38 +0300

    Time: 31 ms (71.37%) | Memory: 18.2 MB (8.81%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -1,10 +1,23 @@
+from collections import Counter
+
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
+        """
+        Determine if a string can be constructed into k palindrome strings.
+
+        :param s: str - The input string.
+        :param k: int - The number of palindrome strings to construct.
+        :return: bool - True if possible, otherwise False.
+        """
+        # If the length of s is less than k, it's impossible to create k palindromes
         if len(s) < k:
             return False
 
-        counter = Counter(s)
-        odd_count = sum(value % 2 == 1 for value in counter.values())
+        # Count the occurrences of each character in the string
+        char_count = Counter(s)
 
-        return odd_count <= k
-        
+        # Count the number of characters with odd occurrences
+        odd_char_count = sum(count % 2 == 1 for count in char_count.values())
+
+        # A valid distribution requires odd_char_count <= k
+        return odd_char_count <= k
