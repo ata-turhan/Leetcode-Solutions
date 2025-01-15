1bd181ff2bc0d70239cd6da4dc59edde9592b4f1
Ata Turhan <ataturhan21@gmail.com>
2025-01-15 07:38:57 +0300

    Time: 1 ms (28.80%) | Memory: 17.8 MB (23.20%) - LeetSync

--- 2509-minimize-xor/minimize-xor.py
+++ 2509-minimize-xor/minimize-xor.py
@@ -1,32 +1,45 @@
 class Solution:
     def minimizeXor(self, num1: int, num2: int) -> int:
-        bin_num1 = bin(num1)[2:]
-        bin_num2 = bin(num2)[2:]
-
-        count_set_bits = bin_num2.count("1")
-
-        x = [0] * len(bin_num1)
-
-        for i in range(len(bin_num1)):
-            if bin_num1[i] == "1":
-                x[i] = 1
-                count_set_bits -= 1
-
-            if count_set_bits == 0:
+        """
+        Minimize the XOR result between num1 and a new number by rearranging the bits of num1
+        such that the new number has the same number of set bits as num2.
+        
+        :param num1: int - The base number for XOR operation.
+        :param num2: int - The number whose set bits count is to be matched.
+        :return: int - The minimized XOR result.
+        """
+        # Convert num1 and num2 to their binary representations
+        binary_num1 = bin(num1)[2:]
+        binary_num2 = bin(num2)[2:]
+        
+        # Count the number of set bits (1s) in num2
+        target_set_bits = binary_num2.count("1")
+        
+        # Create a list to represent the binary bits of the new number
+        new_binary = [0] * len(binary_num1)
+        
+        # Step 1: Set bits in new_binary where binary_num1 has set bits, from left to right
+        for i in range(len(binary_num1)):
+            if binary_num1[i] == "1":
+                new_binary[i] = 1
+                target_set_bits -= 1
+            # Stop if the required number of set bits is reached
+            if target_set_bits == 0:
                 break
 
-        if count_set_bits != 0:
-            for i in range(len(bin_num1) - 1, -1, -1):
-                if bin_num1[i] == "0":
-                    x[i] = 1
-                    count_set_bits -= 1
-
-                if count_set_bits == 0:
+        # Step 2: If there are remaining set bits to place, start filling from the rightmost zeros
+        if target_set_bits > 0:
+            for i in range(len(binary_num1) - 1, -1, -1):
+                if binary_num1[i] == "0":
+                    new_binary[i] = 1
+                    target_set_bits -= 1
+                # Stop if all set bits are placed
+                if target_set_bits == 0:
                     break
 
-            if count_set_bits != 0:
-                x = [1] * count_set_bits + x
+            # Step 3: If still not enough bits, prepend the remaining set bits
+            if target_set_bits > 0:
+                new_binary = [1] * target_set_bits + new_binary
 
-        return int("".join(map(str, x)), 2)
-
-        
+        # Convert the binary representation back to an integer and return
+        return int("".join(map(str, new_binary)), 2)
