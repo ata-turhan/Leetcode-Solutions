1c6e636995ae465d3eb86517182cb5acf0af7b6e
Ata Turhan <ataturhan21@gmail.com>
2025-02-04 19:48:31 -0600

    Time: 0 ms (100.00%) | Memory: 17.6 MB (67.41%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
@@ -1,15 +1,27 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        diff_indices = []
-        
-        for i in range(len(s1)):
-            if s1[i] != s2[i]:
-                diff_indices.append(i)
+        """
+        Checks if two strings can be made equal by swapping exactly one pair of characters.
 
-        if len(diff_indices) == 0:
+        :param s1: First input string.
+        :param s2: Second input string.
+        :return: True if the strings can be made equal by one swap, otherwise False.
+        """
+        mismatch_indices = []  # Stores indices where characters differ
+
+        # Identify mismatched indices
+        for index in range(len(s1)):
+            if s1[index] != s2[index]:
+                mismatch_indices.append(index)
+
+        # If no mismatches, strings are already equal
+        if not mismatch_indices:
             return True
-        elif len(diff_indices) != 2:
+
+        # If more than 2 mismatches, one swap cannot fix it
+        if len(mismatch_indices) != 2:
             return False
 
-        return s1[diff_indices[0]] == s2[diff_indices[1]] and s1[diff_indices[1]] == s2[diff_indices[0]]
-        
+        # Check if swapping the mismatched characters would make the strings equal
+        first_idx, second_idx = mismatch_indices
+        return s1[first_idx] == s2[second_idx] and s1[second_idx] == s2[first_idx]
