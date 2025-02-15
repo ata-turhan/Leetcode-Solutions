1cd7f2b4f7e7b09c4d9f71a9565b0336f03c8660
Ata Turhan <ataturhan21@gmail.com>
2025-02-14 23:14:29 -0600

    Time: 1503 ms (9.23%) | Memory: 18 MB (40.18%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -1,21 +1,41 @@
 class Solution:
     def punishmentNumber(self, n: int) -> int:
-        def can_be_partitioned(num, sum_):
-            def dfs(i, cur_sum, cur_num):
-                if i == len(num):
-                    return cur_sum + int(cur_num) == sum_
-                elif cur_sum > sum_:
+        """
+        Calculates the punishment number by checking if each squared number 
+        can be partitioned into segments summing up to the original number.
+        """
+
+        def can_partition_to_sum(square_str: str, target_sum: int) -> bool:
+            """
+            Determines if the squared number (as a string) can be partitioned 
+            into consecutive parts that sum up to target_sum.
+            """
+
+            def dfs(index: int, current_sum: int, current_part: str) -> bool:
+                """
+                Recursively checks different partition possibilities.
+                - index: Current position in the square_str
+                - current_sum: Sum accumulated so far
+                - current_part: Current number segment being formed
+                """
+                if index == len(square_str):
+                    return current_sum + int(current_part) == target_sum
+                if current_sum > target_sum:
                     return False
-                return dfs(i + 1, cur_sum, cur_num + num[i]) or dfs(i + 1, cur_sum + int(cur_num), num[i])
+
+                # Try continuing the current partition OR finishing it and starting a new partition
+                return (
+                    dfs(index + 1, current_sum, current_part + square_str[index])
+                    or dfs(index + 1, current_sum + int(current_part), square_str[index])
+                )
 
             return dfs(0, 0, "0")
 
-        punishment_number = 0
+        total_punishment = 0
 
-        for num in range(1, n+1):
-            if can_be_partitioned(str(num * num), num):
-                print(num)
-                punishment_number += num * num
+        for num in range(1, n + 1):
+            squared_str = str(num * num)
+            if can_partition_to_sum(squared_str, num):
+                total_punishment += num * num
 
-        return punishment_number
-        
+        return total_punishment
