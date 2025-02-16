dd21cd38a404926840689f23e0e5edd682092d02
Ata Turhan <ataturhan21@gmail.com>
2025-02-15 18:52:25 -0600

    Time: 1115 ms (5.87%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,9 +1,9 @@
-SELECT CASE
-           WHEN s.id % 2 <> 0 AND s.id = (SELECT COUNT(*) FROM Seat) THEN s.id
-           WHEN s.id % 2 = 0 THEN s.id - 1
-           ELSE
-               s.id + 1
-           END AS id,
-       student
+SELECT 
+    CASE 
+        WHEN s.id % 2 <> 0 AND s.id = (SELECT COUNT(*) FROM Seat) THEN s.id -- If last seat is odd, keep it unchanged
+        WHEN s.id % 2 = 0 THEN s.id - 1 -- Swap even id with previous odd id
+        ELSE s.id + 1 -- Swap odd id with next even id
+    END AS id,
+    student
 FROM Seat AS s
-ORDER BY id
+ORDER BY id;
