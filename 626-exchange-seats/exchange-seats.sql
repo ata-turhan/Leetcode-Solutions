0c30677d0ecec8d6aef1caa8d758b650cc0e5649
Ata Turhan <ataturhan21@gmail.com>
2025-02-15 18:49:16 -0600

    Time: 668 ms (39.70%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,9 @@
+SELECT CASE
+           WHEN s.id % 2 <> 0 AND s.id = (SELECT COUNT(*) FROM Seat) THEN s.id
+           WHEN s.id % 2 = 0 THEN s.id - 1
+           ELSE
+               s.id + 1
+           END AS id,
+       student
+FROM Seat AS s
+ORDER BY id
