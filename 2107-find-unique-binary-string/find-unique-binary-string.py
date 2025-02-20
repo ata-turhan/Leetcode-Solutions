fcf931ceefe5048e52f75d6f5507f2ce1e7e7449
Ata Turhan <ataturhan21@gmail.com>
2025-02-19 22:13:13 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (31.88%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,10 +1,16 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        res = []
-        for i, num in enumerate(nums):
-            if num[i] == "0":
-                res.append("1")
+        """
+        Generates a binary string that is not present in the given list `nums`.
+        Uses the Cantor's diagonalization method to guarantee uniqueness.
+        """
+        unique_binary = []  # Stores the result binary string
+
+        for index, binary_str in enumerate(nums):
+            # Flip the diagonal element (i-th character in i-th binary string)
+            if binary_str[index] == "0":
+                unique_binary.append("1")
             else:
-                res.append("0")
-        return "".join(res)
-        
+                unique_binary.append("0")
+
+        return "".join(unique_binary)
