668a1b52e8352949d3d3431088cdf0766115a739
Ata Turhan <ataturhan21@gmail.com>
2025-02-20 22:39:26 -0600

    Time: 9 ms (45.99%) | Memory: 22.5 MB (10.70%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
@@ -1,32 +1,31 @@
+from typing import Optional, Set
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val: int = 0, left: Optional["TreeNode"] = None, right: Optional["TreeNode"] = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class FindElements:
-
     def __init__(self, root: Optional[TreeNode]):
-        def dfs(node):
-            if node.left:
-                node.left.val = 2 * node.val + 1
-                self.nodes.add(node.left.val)
-                dfs(node.left)
-            if node.right:
-                node.right.val = 2 * node.val + 2
-                self.nodes.add(node.right.val)
-                dfs(node.right)
-
+        """Reconstructs the tree and stores all valid node values for quick lookup."""
+        self.nodes: Set[int] = set()
         root.val = 0
-        self.nodes = set()
-        self.nodes.add(root.val)
-        dfs(root)
-        
+        self._recover_tree(root)
+
+    def _recover_tree(self, node: Optional[TreeNode]) -> None:
+        """Performs DFS to reconstruct the tree with correct values."""
+        if not node:
+            return
+        self.nodes.add(node.val)
+        if node.left:
+            node.left.val = 2 * node.val + 1
+            self._recover_tree(node.left)
+        if node.right:
+            node.right.val = 2 * node.val + 2
+            self._recover_tree(node.right)
 
     def find(self, target: int) -> bool:
+        """Checks if a given value exists in the reconstructed tree."""
         return target in self.nodes
-
-
-# Your FindElements object will be instantiated and called as such:
-# obj = FindElements(root)
-# param_1 = obj.find(target)
