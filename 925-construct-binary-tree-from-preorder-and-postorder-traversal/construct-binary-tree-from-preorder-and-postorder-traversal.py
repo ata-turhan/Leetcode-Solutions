fea07b14079d6c0ae315e5d5b6c9d3cfe6c06706
Ata Turhan <ataturhan21@gmail.com>
2025-02-22 20:57:11 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (10.17%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -1,26 +1,32 @@
+from typing import List, Optional
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        def create_tree(preorder, postorder):
-            if not preorder:
-                return None
-            elif len(preorder) == 1:
-                return TreeNode(preorder[0])
-            else:
-                node = TreeNode(preorder[0])
-                left_node = preorder[1]
-                idx_left_node = postorder.index(left_node)
-                right_node = postorder[-2]
-                idx_right_node = preorder.index(right_node)
-                left_size = idx_right_node - 1
-                node.left = create_tree(preorder[1:idx_right_node], postorder[:left_size])
-                node.right = create_tree(preorder[1 + left_size:], postorder[left_size:-1])
-                return node
-
-        return create_tree(preorder, postorder)
+        """Constructs a binary tree from preorder and postorder traversals."""
         
+        def create_tree(pre_idx: int, post_start: int, post_end: int) -> Optional[TreeNode]:
+            """Recursively reconstructs the tree."""
+            if post_start > post_end:
+                return None
+            
+            node = TreeNode(preorder[pre_idx])
+            if post_start == post_end:
+                return node  # Base case for single node
+            
+            left_val = preorder[pre_idx + 1]
+            left_post_idx = postorder.index(left_val)  # Find left subtree size
+            
+            # Construct left and right subtrees
+            node.left = create_tree(pre_idx + 1, post_start, left_post_idx)
+            node.right = create_tree(pre_idx + left_post_idx - post_start + 2, left_post_idx + 1, post_end - 1)
+
+            return node
+        
+        return create_tree(0, 0, len(postorder) - 1)
