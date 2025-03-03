f5efb3e59fda50eaef27ff4a98c88f60438061eb
Ata Turhan <ataturhan21@gmail.com>
2025-03-02 18:28:26 -0600

    Time: 33 ms (54.51%) | Memory: 34.7 MB (33.03%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,7 +1,11 @@
+from typing import List
+
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        """Rearranges the array by placing elements smaller, equal, and larger than the pivot in order."""
         smallers, equals, largers = [], [], []
 
+        # Categorize numbers based on their relation to the pivot
         for num in nums:
             if num < pivot:
                 smallers.append(num)
@@ -10,5 +14,5 @@ class Solution:
             else:
                 equals.append(num)
 
+        # Concatenate lists efficiently
         return smallers + equals + largers
-        
