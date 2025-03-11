036982a5ec5efb032a2a4d597c144ccfbe3c8617
Ata Turhan <ataturhan21@gmail.com>
2025-03-10 19:57:16 -0500

    Time: 323 ms (10.50%) | Memory: 17.7 MB (90.82%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,20 +1,17 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        l = 0
-        frequencies = [0] * 3
-        count = 0
-
-        for r in range(len(s)):
-            char = s[r]
-            frequencies[ord(char) - ord("a")] += 1
-
-            while all(frequency > 0 for frequency in frequencies):
-                count += len(s) - r
-                char = s[l]
-                frequencies[ord(char) - ord("a")] -= 1
-                l += 1
-
-        return count
-
-
+        """Counts the number of substrings containing at least one of each character 'a', 'b', and 'c'."""
         
+        left_pointer = 0
+        char_frequencies = [0] * 3
+        substring_count = 0
+
+        for right_pointer in range(len(s)):
+            char_frequencies[ord(s[right_pointer]) - ord("a")] += 1
+
+            while all(freq > 0 for freq in char_frequencies):
+                substring_count += len(s) - right_pointer
+                char_frequencies[ord(s[left_pointer]) - ord("a")] -= 1
+                left_pointer += 1
+
+        return substring_count
