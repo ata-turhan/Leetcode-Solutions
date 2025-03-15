41d8f6620ab325c845cf9148888ab6675a9914cb
Ata Turhan <ataturhan21@gmail.com>
2025-03-14 21:56:45 -0500

    Time: 1557 ms (11.63%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,6 +1,9 @@
+WITH distinct_product_count AS (
+    SELECT COUNT(DISTINCT product_key) AS total_products
+    FROM product
+)
 SELECT c.customer_id
-FROM customer AS c
+FROM customer c
+JOIN distinct_product_count dpc 
 GROUP BY c.customer_id
-HAVING COUNT(DISTINCT c.product_key) = (
-    SELECT COUNT(DISTINCT p.product_key) FROM product AS p
-);
+HAVING COUNT(DISTINCT c.product_key) = MAX(dpc.total_products);
