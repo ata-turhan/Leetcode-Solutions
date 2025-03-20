3dbe9b7fb892314cbebe14ca777ce7cd86dc2237
Ata Turhan <ataturhan21@gmail.com>
2025-03-19 21:33:48 -0500

    Time: 1698 ms (83.79%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -0,0 +1,5 @@
+SELECT u.user_id as buyer_id, u.join_date, count(o.order_id) as 'orders_in_2019'
+FROM users u
+LEFT JOIN Orders o
+ON o.buyer_id=u.user_id AND YEAR(order_date)='2019'
+GROUP BY u.user_id
