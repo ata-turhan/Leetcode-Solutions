8cc0965a5883de79a5c796589d29eb519458d7d7
Ata Turhan <ataturhan21@gmail.com>
2025-03-25 21:00:22 -0500

    Time: 328 ms (98.72%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,7 @@
-SELECT e1.name 
+SELECT 
+    e1.name
 FROM employee AS e1
 JOIN employee AS e2 
-ON e1.id = e2.managerId 
-GROUP BY e1.id 
+    ON e1.id = e2.managerId
+GROUP BY e1.id, e1.name
 HAVING COUNT(*) >= 5;
