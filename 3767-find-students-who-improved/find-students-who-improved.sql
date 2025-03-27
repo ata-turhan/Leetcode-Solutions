742af53ea4c2d54d1de8fddbcc44956290f36a16
Ata Turhan <ataturhan21@gmail.com>
2025-03-26 20:21:13 -0500

    Time: 289 ms (97.19%) | Memory: 0B (100.00%) - LeetSync

--- 3767-find-students-who-improved/find-students-who-improved.sql
+++ 3767-find-students-who-improved/find-students-who-improved.sql
@@ -1,8 +1,24 @@
-with ranks as (
-    select student_id, subject, score, exam_date, 
-    row_number() over ( partition by student_id, subject order by exam_date asc ) as ascending,
-    row_number() over ( partition by student_id, subject order by exam_date desc ) as descending
-    from scores
+WITH ranked_scores AS (
+    SELECT 
+        student_id, 
+        subject, 
+        score, 
+        exam_date, 
+        ROW_NUMBER() OVER (PARTITION BY student_id, subject ORDER BY exam_date ASC) AS ascending,
+        ROW_NUMBER() OVER (PARTITION BY student_id, subject ORDER BY exam_date DESC) AS descending
+    FROM scores
 )
-select r1. student_id, r2.subject, r1.score as first_score, r2.score as latest_score from ranks as r1 join ranks as r2 on r1.student_id = r2.student_id and  r1.subject = r2.subject
-where r1.ascending = 1 and r2.descending = 1 and r1.score < r2.score
+
+SELECT 
+    r1.student_id, 
+    r1.subject, 
+    r1.score AS first_score, 
+    r2.score AS latest_score
+FROM ranked_scores AS r1
+JOIN ranked_scores AS r2 
+    ON r1.student_id = r2.student_id 
+   AND r1.subject = r2.subject
+WHERE 
+    r1.ascending = 1 
+    AND r2.descending = 1 
+    AND r1.score < r2.score;
