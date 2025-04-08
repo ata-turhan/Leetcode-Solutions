2ea0bcdc88b7b5e8518cd7597bbf60457e26db3b
Ata Turhan <ataturhan21@gmail.com>
2025-04-07 20:43:44 -0500

    Time: 7 ms (33.99%) | Memory: 17.9 MB (48.49%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,21 +1,33 @@
+from typing import List
+from collections import Counter
+
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        counter = Counter(nums)
-        non_distinct_counts = sum(val > 1 for val in counter.values())
-        num_operations = 0
-        nums.reverse()
+        """
+        Computes the minimum number of operations needed to remove duplicates from nums
+        using a batch of 3-element deletions per operation. Each operation removes 3 elements.
+        Returns the number of operations required to make all elements distinct.
+        """
 
-        while non_distinct_counts > 0:
+        frequency = Counter(nums)
+        duplicate_count = sum(1 for count in frequency.values() if count > 1)
+        num_operations = 0
+        nums = nums[::-1]  # Reverse to simulate popping from the end
+
+        while duplicate_count > 0:
             if len(nums) <= 3:
                 num_operations += 1
                 break
-            else:
-                for _ in range(3):
-                    removed_val = nums.pop()
-                    counter[removed_val] -= 1
-                    if counter[removed_val] == 1:                 
-                        non_distinct_counts -= 1
-                num_operations += 1
+
+            # Remove up to 3 elements from the end
+            for _ in range(3):
+                if not nums:
+                    break
+                val = nums.pop()
+                frequency[val] -= 1
+                if frequency[val] == 1:
+                    duplicate_count -= 1
+
+            num_operations += 1
 
         return num_operations
-        
