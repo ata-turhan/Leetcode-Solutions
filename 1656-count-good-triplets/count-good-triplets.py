8af409e7929bb963a20922bf8db49fe6031a2171
Ata Turhan <ataturhan21@gmail.com>
2025-04-13 20:30:58 -0500

    Time: 184 ms (82.80%) | Memory: 17.6 MB (85.75%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.py
+++ 1656-count-good-triplets/count-good-triplets.py
@@ -1,12 +1,23 @@
+from typing import List
+
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
-        count_good_triplets = 0
+        """
+        Counts the number of good triplets (i, j, k) such that:
+        0 <= i < j < k < len(arr) and
+        |arr[i] - arr[j]| <= a,
+        |arr[j] - arr[k]| <= b,
+        |arr[i] - arr[k]| <= c
+        """
         n = len(arr)
+        good_triplet_count = 0
 
-        for i in range(n):
-            for j in range(i + 1, n):
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                if abs(arr[i] - arr[j]) > a:
+                    continue
                 for k in range(j + 1, n):
-                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
-                        count_good_triplets += 1
+                    if abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                        good_triplet_count += 1
 
-        return count_good_triplets
+        return good_triplet_count
