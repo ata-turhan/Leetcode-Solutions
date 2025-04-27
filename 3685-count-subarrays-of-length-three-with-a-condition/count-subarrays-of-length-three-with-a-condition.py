600a16718c103ceec525b718c0363c544bedd72e
Ata Turhan <ataturhan21@gmail.com>
2025-04-26 20:16:22 -0500

    Time: 7 ms (58.57%) | Memory: 18 MB (7.35%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.py
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.py
@@ -1,10 +1,16 @@
+from typing import List
+
 class Solution:
     def countSubarrays(self, nums: List[int]) -> int:
+        """
+        Counts the number of subarrays of length 3 where
+        nums[i+1] is the half of sum of nums[i] and nums[i+2].
+        """
         subarray_count = 0
+        n = len(nums)
 
-        for i in range(len(nums)-2):
-            if (nums[i] + nums[i + 2]) == nums[i + 1] / 2:
+        for i in range(n - 2):
+            if nums[i] + nums[i + 2] == nums[i + 1] / 2:
                 subarray_count += 1
 
         return subarray_count
-        
