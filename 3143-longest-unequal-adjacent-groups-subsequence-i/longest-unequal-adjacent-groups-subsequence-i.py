635a1dfbf77625cc5f2972daff7127231663de26
Ata Turhan <ataturhan21@gmail.com>
2025-05-14 20:41:20 -0500

    Time: 1 ms (35.81%) | Memory: 18 MB (22.15%) - LeetSync

--- 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.py
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.py
@@ -1,37 +1,27 @@
+from typing import List
+
 class Solution:
     def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
-        start_with_zero = []
-        start_with_one = []
+        """
+        Builds the longest subsequence of words such that the group labels alternate between 0 and 1.
+        Returns the sequence (not the indices) that forms the longest alternating pattern.
+        """
+        subseq_starting_with_0 = []
+        subseq_starting_with_1 = []
 
-        for i, num in enumerate(groups):
-            if num == 1:
-                if not start_with_one:
-                    start_with_one.append(i)
-                else:
-                    if groups[start_with_one[-1]] == 0:
-                        start_with_one.append(i)
-                if start_with_zero and groups[start_with_zero[-1]] == 0:
-                    start_with_zero.append(i)
-            elif num == 0:
-                if not start_with_zero:
-                    start_with_zero.append(i)
-                else:
-                    if groups[start_with_zero[-1]] == 1:
-                        start_with_zero.append(i)
-                if start_with_one and groups[start_with_one[-1]] == 1:
-                    start_with_one.append(i)
+        for i, group in enumerate(groups):
+            # Extend subsequence starting with 0
+            if not subseq_starting_with_0 or groups[subseq_starting_with_0[-1]] != group:
+                subseq_starting_with_0.append(i)
 
-        res = []
-        longest_array = []
-        if len(start_with_zero) > len(start_with_one):
-            longest_array = start_with_zero
-        else:
-            longest_array = start_with_one
+            # Extend subsequence starting with 1
+            if not subseq_starting_with_1 or groups[subseq_starting_with_1[-1]] != group:
+                subseq_starting_with_1.append(i)
 
-        for i in longest_array:
-            res.append(words[i])
+        # Choose the longer alternating subsequence
+        longest_indices = (
+            subseq_starting_with_0 if len(subseq_starting_with_0) >= len(subseq_starting_with_1)
+            else subseq_starting_with_1
+        )
 
-        return res
-
-
-        
+        return [words[i] for i in longest_indices]
