4569ae981e5fb0e5a5ba4c7945bda9cbfbf920a6
Ata Turhan <ataturhan21@gmail.com>
2025-06-15 20:44:25 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (59.91%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.py
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.py
@@ -1,12 +1,17 @@
+from typing import List
+
 class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
-        max_dif = -1
-        max_so_far = nums[-1]
+        """
+        Finds the maximum difference (nums[j] - nums[i]) such that j > i and nums[j] > nums[i].
+        If no such pair exists, returns -1.
+        """
+        max_diff: int = -1
+        max_from_right: int = nums[-1]
 
         for i in range(len(nums) - 2, -1, -1):
-            if nums[i] < max_so_far:
-                max_dif = max(max_dif, max_so_far - nums[i])
-            max_so_far = max(max_so_far, nums[i])
-            
-        return max_dif
-        
+            if nums[i] < max_from_right:
+                max_diff = max(max_diff, max_from_right - nums[i])
+            max_from_right = max(max_from_right, nums[i])
+
+        return max_diff
