5c40aa2439db34a3b415787e9cb158e9a3fa4f22
Ata Turhan <ataturhan21@gmail.com>
2025-06-18 21:12:04 -0500

    Time: 91 ms (46.19%) | Memory: 29 MB (73.61%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.py
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.py
@@ -1,16 +1,19 @@
+from typing import List
+
 class Solution:
     def partitionArray(self, nums: List[int], k: int) -> int:
+        """
+        Partitions the array into the minimum number of subsequences
+        such that the absolute difference between the minimum and 
+        maximum of each subsequence is less than or equal to k.
+        """
         nums.sort()
-        num_subseq = 1
-
-        min_val = nums[0]
+        subsequence_count = 1
+        subsequence_min = nums[0]
 
         for i in range(1, len(nums)):
-            if nums[i] > min_val + k:
-                num_subseq += 1
-                min_val = nums[i]
+            if nums[i] > subsequence_min + k:
+                subsequence_count += 1
+                subsequence_min = nums[i]
 
-        return num_subseq
-
-
-        
+        return subsequence_count
