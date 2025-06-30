9fde191b75e5c015c61d671ccc4796ccb4a2f0d8
Ata Turhan <ataturhan21@gmail.com>
2025-06-29 20:35:31 -0500

    Time: 17 ms (92.57%) | Memory: 19.2 MB (46.65%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
@@ -1,12 +1,17 @@
+from typing import List
+from collections import Counter
+
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        counter = Counter(nums)
+        """
+        Find the length of the longest harmonious subsequence.
+        A harmonious array is where the difference between the maximum and minimum elements is exactly 1.
+        """
+        freq: Counter[int] = Counter(nums)
+        max_length: int = 0
 
-        longest_subsequence = 0
+        for num in freq:
+            if num + 1 in freq:
+                max_length = max(max_length, freq[num] + freq[num + 1])
 
-        for num in counter:
-            if num + 1 in counter:
-                longest_subsequence = max(longest_subsequence, counter[num] + counter.get(num + 1, 0))
-
-        return longest_subsequence
-        
+        return max_length
