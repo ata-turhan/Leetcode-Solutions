561dc9b047c41fcfbd0c5ecfeb718095380e36ae
Ata Turhan <ataturhan21@gmail.com>
2025-06-30 21:10:49 -0500

    Time: 46 ms (15.21%) | Memory: 17.5 MB (92.68%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
@@ -1,16 +1,22 @@
+from typing import List
+
 class Solution:
     def possibleStringCount(self, word: str) -> int:
-        total_combs = 1
-        cur_freq = 1
-        
+        """
+        Count the total number of substrings where adjacent characters are the same.
+        For each consecutive sequence of identical characters of length `n`, 
+        there are (n - 1) such substrings.
+        """
+        total_count: int = 1  # Start with 1 for the first character
+        run_length: int = 1
+
         for i in range(1, len(word)):
             if word[i] == word[i - 1]:
-                cur_freq += 1
+                run_length += 1
             else:
-                total_combs += cur_freq - 1
-                cur_freq = 1
+                total_count += run_length - 1
+                run_length = 1
 
-        total_combs += cur_freq - 1
+        total_count += run_length - 1  # Account for the last run
 
-        return total_combs
-        
+        return total_count
