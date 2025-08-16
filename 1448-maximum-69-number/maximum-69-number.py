2aff9841e589589187a0b5783ff1f2795023b90b
Ata Turhan <ataturhan21@gmail.com>
2025-08-15 19:56:38 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (28.83%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.py
+++ 1448-maximum-69-number/maximum-69-number.py
@@ -1,10 +1,13 @@
 class Solution:
-    def maximum69Number (self, num: int) -> int:
-        arr = list(str(num))
-        for i in range(len(arr)):
-            if arr[i] == "6":
-                arr[i] = "9"
-                break
+    def maximum69Number(self, num: int) -> int:
+        """
+        Converts the first '6' in the number to a '9' to form the largest possible number.
+        """
+        digits = list(str(num))
 
-        return int("".join(arr))
-        
+        for i, digit in enumerate(digits):
+            if digit == '6':
+                digits[i] = '9'
+                break  # Only convert the first '6' found
+
+        return int("".join(digits))
